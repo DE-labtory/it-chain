021abcb059602c128e88a99ef7233154c03ea489
yojkim <youthful2016@gmail.com>
2018-01-07 23:49:38 +0900

    add base interface of cryptography

--- auth/crypto.go
+++ auth/crypto.go
@@ -1,1 +1,19 @@
 package auth
+
+import (
+	"crypto"
+)
+
+type Key interface {}
+
+type SignerOpt interface {
+	crypto.SignerOpts
+}
+
+type Crypto interface {
+
+	Sign(key Key, data []byte, opt SignerOpt) ([]byte, error)
+
+	Verify(key Key, sig []byte, data []byte, opts SignerOpt) (bool, error)
+
+}
