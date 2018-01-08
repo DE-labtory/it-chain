5a8d3b0bedb7115f61b955005e19a506483e8730
Emperorhan <iny27@naver.com>
2018-01-09 04:35:57 +0900

    changed some blockchain struct Added ComputeSHA256 function to util.go

--- service/blockchain/blockchain_test.go
+++ service/blockchain/blockchain_test.go
@@ -3,13 +3,30 @@ package blockchain
 import (
 	"github.com/magiconair/properties/assert"
 	"testing"
+	"fmt"
 )
 
 func TestCreateNewBlockChainTest(t *testing.T){
 
 	var blockChains = CreateNewBlockChain(defaultChannelName,defaultPeerId)
 
-	assert.Equal(t,0,len(blockChains.Blocks))
-	assert.Equal(t,defaultPeerId,blockChains.Header.peerID)
-	assert.Equal(t,defaultChannelName,blockChains.Header.channelName)
+	assert.Equal(t,0,len(blockChains.blocks))
+	assert.Equal(t,defaultPeerId,blockChains.header.peerID)
+	assert.Equal(t,defaultChannelName,blockChains.header.channelName)
+
+
+}
+
+func TestTxTest(t *testing.T) {
+	blk := Block{}
+	tx := Transaction{}
+	tx.txData = TxData{"ss","dd", Params{3, "func", []string{"10", "6"}}, "ff"}
+	tx.GenerateHash()
+
+	if blk.PutTranscation(tx) == true{
+		idx, err := blk.FindTransactionIndex(tx.transactionHash)
+		fmt.Println(idx, err)
+		fmt.Printf("%x\n", blk.transactions[idx].transactionHash)
+	}
+
 }
--- service/blockchain/transaction.go
+++ service/blockchain/transaction.go
@@ -2,6 +2,8 @@ package blockchain
 
 import (
 	"time"
+	"it-chain/common"
+	"strings"
 )
 
 type TransactionStatus int
@@ -10,10 +12,6 @@ type TransactionType int
 type FunctionType string
 
 const (
-
-	transactionUnconfirmed TransactionStatus = 0 + iota //unconfirmed block
-	transactionConfirmed
-
 	invoke TxDataType = "invoke"
 	query TxDataType = "query"
 
@@ -25,26 +23,68 @@ const (
 )
 
 type Params struct {
-	paramsType int
-	function   FunctionType
-	args       []string
+	paramsType	int
+	function 	FunctionType
+	args     	[]string
 }
 
 type TxData struct {
-	jsonrpc string
-	method  TxDataType
-	params  Params
-	contractID string
+	jsonrpc		string
+	method 		TxDataType
+	params 		Params
+	contractID	string
 }
 
 type Transaction struct {
 	invokePeerID      string
 	transactionID     string
-	transactionStatus TransactionStatus
+	transactionStatus Status
 	transactionType   TransactionType
 	publicKey         []byte
 	signature         []byte
-	transactionHash   string
+	transactionHash   [32]uint8
 	timeStamp         time.Time
 	txData            TxData
+}
+
+func (tx *Transaction) init(peer_id string, tx_id string, status Status, tx_type TransactionType, key []byte, sign []byte, hash [32]uint8, t time.Time, data *TxData){
+	tx.invokePeerID 		= peer_id
+	tx.transactionID 		= tx_id
+	tx.transactionStatus	= status
+	tx.transactionType		= tx_type
+	tx.publicKey			= key
+	tx.signature			= sign
+	tx.transactionHash		= hash
+	tx.timeStamp			= t
+	tx.txData				= *data
+}
+
+func (tx *Transaction) GenerateHash() error{
+	sum_string := []string{tx.invokePeerID, tx.txData.jsonrpc, string(tx.txData.method), string(tx.txData.params.function)}
+	for _, str := range tx.txData.params.args{
+		sum_string = append(sum_string, str)
+	}
+	str := strings.Join(sum_string, ",")
+	tx.transactionHash = common.ComputeSHA256([]byte(str))
+	return nil
+}
+
+func (tx Transaction) GenerateTransactionHash() [32]uint8{
+	sum_string := []string{tx.invokePeerID, tx.txData.jsonrpc, string(tx.txData.method), string(tx.txData.params.function)}
+	for _, str := range tx.txData.params.args{
+		sum_string = append(sum_string, str)
+	}
+	str := strings.Join(sum_string, ",")
+	return common.ComputeSHA256([]byte(str))
+}
+
+func (tx *Transaction) GetTxHash() [32]uint8{
+	return tx.transactionHash
+}
+
+func (tx Transaction) Validate() bool{
+	if tx.GenerateTransactionHash() != tx.GetTxHash(){
+		return false
+	}
+	return true
 }
