dbc8b4357dbb69a184407102619d15e865b579c2
jun <leebduk@gmail.com>
2018-01-16 00:52:47 +0900

    add state diagram and sample

--- network/comm/comm_impl_test.go
+++ network/comm/comm_impl_test.go
@@ -69,6 +69,8 @@ func TestCommImpl_CreateConn(t *testing.T) {
 }
 
 func TestCommImpl_Send(t *testing.T) {
+	counter = 0
+
 	peer1 := MockCreateNewPeerInfo("test1")
 	server1, listner1 := ListenMockServerWithPeer(*peer1)
 
@@ -90,16 +92,59 @@ func TestCommImpl_Send(t *testing.T) {
 	envelope := &pb.Envelope{Signature:[]byte("123")}
 
 	comm.Send(*envelope,nil, *peer1, *peer2)
+	comm.Send(*envelope,nil, *peer2)
 
 	time.Sleep(3*time.Second)
 
-	assert.Equal(t,2,counter)
+	assert.Equal(t,3,counter)
 }
 
 func TestCommImpl_Stop(t *testing.T) {
+	counter = 0
+	peer1 := MockCreateNewPeerInfo("test1")
+	server1, listner1 := ListenMockServerWithPeer(*peer1)
 
+	peer2 := MockCreateNewPeerInfo("test2")
+	peer2.Port = "6666"
+	server2, listner2 := ListenMockServerWithPeer(*peer2)
+
+	defer func(){
+		server1.Stop()
+		listner1.Close()
+		server2.Stop()
+		listner2.Close()
+	}()
+
+	comm := NewCommImpl()
+	comm.CreateConn(*peer1)
+	comm.CreateConn(*peer2)
+
+	comm.Stop()
+
+	assert.Equal(t,0,comm.Size())
 }
 
 func TestCommImpl_Close(t *testing.T) {
 
+	peer1 := MockCreateNewPeerInfo("test1")
+	server1, listner1 := ListenMockServerWithPeer(*peer1)
+
+	peer2 := MockCreateNewPeerInfo("test2")
+	peer2.Port = "6666"
+	server2, listner2 := ListenMockServerWithPeer(*peer2)
+
+	defer func(){
+		server1.Stop()
+		listner1.Close()
+		server2.Stop()
+		listner2.Close()
+	}()
+
+	comm := NewCommImpl()
+	comm.CreateConn(*peer1)
+	comm.CreateConn(*peer2)
+
+	comm.Close(*peer1)
+
+	assert.Equal(t,1,comm.Size())
 }
