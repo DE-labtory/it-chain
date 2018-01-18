45a76fb36be898f404725db26eca8830ef4faed9
yojkim <youthful2016@gmail.com>
2018-01-18 21:09:06 +0900

    implement private store public / private key function

--- auth/keyStorer.go
+++ auth/keyStorer.go
@@ -2,6 +2,9 @@ package auth
 
 import (
 	"errors"
+	"path/filepath"
+	"io/ioutil"
+	"encoding/hex"
 )
 
 type keyStorer struct {
@@ -14,24 +17,54 @@ func (ks *keyStorer) Store(key Key) (err error) {
 		return errors.New("Failed to get Key Data")
 	}
 
-	switch key.(type) {
+	switch k := key.(type) {
 	case *rsaPrivateKey:
+		err = ks.storePrivateKey(k)
 	case *rsaPublicKey:
+		err = ks.storePublicKey(k)
 	case *ecdsaPrivateKey:
+		err = ks.storePrivateKey(k)
 	case *ecdsaPublicKey:
+		err = ks.storePublicKey(k)
 	default:
 		return errors.New("Unspported Key Type.")
 	}
 	
+	return
+}
+
+func (ks *keyStorer) storePublicKey(key Key) (err error) {
+
+	data, err := PublicKeyToPem(key)
+	if err != nil {
+		return
+	}
+
+	err = ioutil.WriteFile(ks.getFullPath(hex.EncodeToString(key.SKI()), "pub"), data, 0700)
+	if err != nil {
+		return
+	}
+
 	return nil
 }
 
-func storePublicKey(key Key) (err error) {
+func (ks *keyStorer) storePrivateKey(key Key) (err error) {
+
+	data, err := PrivateKeyToPem(key)
+	if err != nil {
+		return
+	}
+
+	err = ioutil.WriteFile(ks.getFullPath(hex.EncodeToString(key.SKI()), "pri"), data, 0700)
+	if err != nil {
+		return
+	}
+
 	return nil
 }
 
-func storePrivateKey(key Key) (err error) {
-	return nil
+func (ks *keyStorer) getFullPath(alias, suffix string) string {
+	return filepath.Join(ks.path, alias + "_" + suffix)
 }
 
 
