b427b9578f2a72ea00c30dfb703b033bdfffc229
yojkim <youthful2016@gmail.com>
2018-01-25 21:35:37 +0900

    implement default logic of  LoadKey function in keyManager

--- auth/keyManager.go
+++ auth/keyManager.go
@@ -6,32 +6,56 @@ import (
 	"io/ioutil"
 	"encoding/hex"
 	"os"
+	"reflect"
+	"strings"
 )
 
 type keyManager struct {
 	path string
+
+	keyImporters map[reflect.Type]keyImporter
 }
 
-func (km *keyManager) Store(key Key) (err error) {
+func (km *keyManager) Init(path string) {
 
-	if key == nil {
-		return errors.New("Failed to get Key Data")
+	if len(path) == 0 {
+		km.path = "/KeyRepository"
+	} else {
+		km.path = path
 	}
 
-	switch k := key.(type) {
-	case *rsaPrivateKey:
-		err = km.storePrivateKey(k)
-	case *rsaPublicKey:
-		err = km.storePublicKey(k)
-	case *ecdsaPrivateKey:
-		err = km.storePrivateKey(k)
-	case *ecdsaPublicKey:
-		err = km.storePublicKey(k)
-	default:
-		return errors.New("Unspported Key Type.")
+	keyImporters := make(map[reflect.Type]keyImporter)
+	keyImporters[reflect.TypeOf(&RSAPrivateKeyImporterOpts{})] = &rsaPrivateKeyImporter{}
+	keyImporters[reflect.TypeOf(&RSAPublicKeyImporterOpts{})] = &rsaPublicKeyImporter{}
+	keyImporters[reflect.TypeOf(&ECDSAPrivateKeyImporterOpts{})] = &ecdsaPrivateKeyImporter{}
+	keyImporters[reflect.TypeOf(&ECDSAPublicKeyImporterOpts{})] = &ecdsaPublicKeyImporter{}
+
+	km.keyImporters = keyImporters
+
+}
+
+func (km *keyManager) Store(keys... Key) (err error) {
+
+	if len(keys) == 0 {
+		return errors.New("Input values should not be NIL")
 	}
-	
-	return
+
+	for _, key := range keys {
+		switch k := key.(type) {
+		case *rsaPrivateKey:
+			err = km.storePrivateKey(k)
+		case *rsaPublicKey:
+			err = km.storePublicKey(k)
+		case *ecdsaPrivateKey:
+			err = km.storePrivateKey(k)
+		case *ecdsaPublicKey:
+			err = km.storePublicKey(k)
+		default:
+			return errors.New("Unspported Key Type.")
+		}
+	}
+
+	return nil
 }
 
 func (km *keyManager) storePublicKey(key Key) (err error) {
@@ -78,19 +102,78 @@ func (km *keyManager) storePrivateKey(key Key) (err error) {
 	return nil
 }
 
-func (km *keyManager) Load(alias string) (key interface{}, err error) {
-	return nil, nil
+func (km *keyManager) LoadKey() (pub, pri Key, err error) {
+
+	if _, err := os.Stat(km.path); os.IsNotExist(err) {
+		return nil, nil, errors.New("Keys are not exist")
+	}
+
+	files, err := ioutil.ReadDir(km.path)
+	if err != nil {
+		return nil, nil, errors.New("Failed to read key repository directory")
+	}
+
+	for _, file := range files {
+
+		suffix, valid := km.getSuffix(file.Name())
+		if valid == true {
+			alias := strings.Split(file.Name(), "_")[0]
+			switch suffix {
+			case "pri":
+				data, err := km.loadPrivateKey(alias)
+				if err != nil {
+					return nil, nil, err
+				}
+
+				pri, err = km.importKey(data)
+				if err != nil {
+					return nil, nil, err
+				}
+			case "pub":
+				data, err := km.loadPublicKey(alias)
+				if err != nil {
+					return nil, nil, err
+				}
+
+				pri, err = km.importKey(data)
+				if err != nil {
+					return nil, nil, err
+				}
+			}
+		}
+	}
+
+	return
+
 }
 
-func (km *keyManager) loadPublicKey(alias string) (key interface{}, err error) {
+func (km *keyManager) loadPrivateKey(alias string) (data interface{}, err error) {
 
 }
 
-func (km *keyManager) loadPrivateKey(alias string) (key interface{}, err error) {
+func (km *keyManager) loadPublicKey(alias string) (data interface{}, err error) {
 
 }
 
-func (km *keyManager) getSuffix()
+func (km *keyManager) importKey(data interface{}) (key Key, err error) {
+
+	if data == nil {
+		return nil, errors.New("Data should not be NIL")
+	}
+
+}
+
+func (km *keyManager) getSuffix(name string) (suffix string, valid bool) {
+
+	if strings.HasSuffix(name, "pri") {
+		return "pri", true
+	} else if strings.HasSuffix(name, "pub") {
+		return "pub", true
+	}
+
+	return "", false
+
+}
 
 func (km *keyManager) getFullPath(alias, suffix string) (path string, err error) {
 	if _, err := os.Stat(km.path); os.IsNotExist(err) {
