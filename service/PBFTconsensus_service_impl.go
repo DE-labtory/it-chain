d27995d57c0d96c542ea5c0c7d976f1799f9839a
jun <leebduk@gmail.com>
2018-02-10 17:16:57 +0900

    add builder pattern and consensusResolve done

--- service/PBFTconsensus_service_impl.go
+++ service/PBFTconsensus_service_impl.go
@@ -50,11 +50,11 @@ func (cs *PBFTConsensusService) StartConsensus(block *domain.Block){
 
 	//set consensus message to broadcast
 	sequenceID := time.Now().UnixNano()
-	preprepareConsensusMessage := domain.NewConsesnsusMessage(consensusState.ID,cs.view.ID,sequenceID,consensusState.Block,cs.peerID,domain.PreprepareMsg)
+	preprepareConsensusMessage := domain.NewConsesnsusMessage(consensusState.ID,*cs.view,sequenceID,consensusState.Block,cs.peerID,domain.PreprepareMsg)
+	cs.broadcastMessage(preprepareConsensusMessage)
+	consensusState.CurrentStage = domain.Prepared
 
 	cs.Unlock()
-
-	cs.broadcastMessage(preprepareConsensusMessage)
 }
 
 func (cs *PBFTConsensusService) GetCurrentConsensusState() map[string]*domain.ConsensusState{
@@ -67,7 +67,6 @@ func (cs *PBFTConsensusService) StopConsensus(){
 
 //consensusMessage가 들어옴
 //todo FromConsensusProtoMessage에서 block변환도 해야함
-//todo 언제 Message를 무시해야 하는가 일단은 time laps는 5분
 //todo time을 config로 부터 읽어야함
 //todo 다음 block이 먼저 들어올 경우 고려해야함,
 //todo 블록의 높이와 이전 블록 해시가 올바른지 확인
@@ -87,45 +86,64 @@ func (cs *PBFTConsensusService) ReceiveConsensusMessage(outterMessage comm.Outte
 	elapsed := time.Since(t)
 
 	if math.Abs(elapsed.Minutes()) > 5.0 {
-		logger_pbftservice.Errorln("time over(5min)")
+		logger_pbftservice.Errorln("time over (5min)")
 		return
 	}
 
 	//2 consensus id check
-	consensusID := consensusMessage.ConsensusID
-	msgType := consensusMessage.MsgType
-	consensusState, ok := cs.consensusStates[consensusID]
-
 	cs.Lock()
+	defer cs.Unlock()
+
+	consensusID := consensusMessage.ConsensusID
+	consensusState, ok := cs.consensusStates[consensusID]
 
 	if !ok{
 		//consensus state생성
-		//prepremessage인 경우에만 block과 view를 세팅
+		//prepremessage인 경우에만 block과 view, stage를 세팅
 		//var newConsensusState *domain.ConsensusState
-		//if consensusMessage.MsgType == domain.PreprepareMsg{
-		//	newConsensusState = domain.NewConsensusState(&consensusMessage.View,consensusMessage.ConsensusID,consensusMessage.Block,domain.Stage(msgType),cs.HandleEndConsensus,300)
-		//}
-		//
-		//cs.consensusStates[newConsensusState.ID] = newConsensusState
+		consensusStateBuilder := domain.NewConsensusStateBuilder()
+
+		if consensusMessage.MsgType == domain.PreprepareMsg{
+
+			consensusState = consensusStateBuilder.
+				ConsensusID(consensusMessage.ConsensusID).
+				CurrentStage(domain.Prepared).
+				View(&consensusMessage.View).
+				Block(consensusMessage.Block).
+				EndConsensusHandler(cs.HandleEndConsensus).
+				Period(300).Build()
+
+		}else{
+
+			consensusState = consensusStateBuilder.
+				ConsensusID(consensusMessage.ConsensusID).
+				EndConsensusHandler(cs.HandleEndConsensus).
+				Period(300).Build()
+		}
+
+		cs.consensusStates[consensusState.ID] = consensusState
 	}
 
-	//if !ok{
-	//	//이미 state가 존재함
-	//	//prepare or commit message
-	//	consensusState.AddMessage(consensusMessage)
-	//}else{
-	//	//preprepare message를 받는경우
-	//	//id가 다르면 check안함
-	//	//block check
-	//	newConsensusState := domain.NewConsensusState(cs.view.ID,consensusMessage.ConsensusID,nil,domain.Stage(msgType),cs.HandleEndConsensus)
-	//	newConsensusState.AddMessage(consensusMessage)
-	//	cs.consensusStates[newConsensusState.ID] = newConsensusState
-	//}
-	cs.Unlock()
+	consensusState.AddMessage(consensusMessage)
+
+	//1. prepare stage && prepare message가 전체의 2/3이상 -> commitMsg전파
+
+	if consensusState.CurrentStage == domain.Prepared && consensusState.PrepareReady(){
+		sequenceID := time.Now().UnixNano()
+		commitConsensusMessage := domain.NewConsesnsusMessage(consensusState.ID,*cs.view,sequenceID,consensusState.Block,cs.peerID,domain.CommitMsg)
+		cs.broadcastMessage(commitConsensusMessage)
+		consensusState.CurrentStage = domain.Committed
+	}
+
+	//2. commit state && commit message가 전체의 2/3이상 -> 블록저장
+	if consensusState.CurrentStage == domain.Committed && consensusState.CommitReady(){
+		//block 저장
+		//todo block에 저장
+	}
 }
 
 func (cs *PBFTConsensusService) HandleEndConsensus(consensusState domain.ConsensusState){
-
+	
 }
 
 //tested
