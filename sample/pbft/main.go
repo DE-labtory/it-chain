62a22ed2b098aed749ff1e5ef0a10db328fffd77
jun <leebduk@gmail.com>
2018-02-13 00:52:52 +0900

    hash part move to inside sign function

--- sample/pbft/main.go
+++ sample/pbft/main.go
@@ -16,6 +16,9 @@ import (
 	"it-chain/network/comm/publisher"
 	"time"
 	"github.com/urfave/cli"
+	"os"
+	"strings"
+	"github.com/rs/xid"
 )
 
 var View = &domain.View{
@@ -42,14 +45,22 @@ func NewNode(peerInfo *domain.Peer) *Node{
 	node := &Node{}
 	node.myInfo = peerInfo
 
-	crypto, err := auth.NewCrypto("./"+node.myInfo.GetEndPoint())
+	crypto, err := auth.NewCrypto("./sample/pbft/"+node.myInfo.GetEndPoint())
+	_, pub ,err := crypto.GenerateKey(&auth.RSAKeyGenOpts{})
+
+	if err !=nil{
+		log.Println(err)
+	}
+
+	node.myInfo.PubKey = pub.SKI()
+
+	log.Println(node.myInfo.PubKey)
 
 	if err != nil{
 		panic("fail to create keys")
 	}
 
 	connectionManager := comm.NewConnectionManagerImpl(crypto)
-
 
 	//consensusService
 	consensusService := service.NewPBFTConsensusService(View,connectionManager,nil)
@@ -67,6 +78,7 @@ func NewNode(peerInfo *domain.Peer) *Node{
 	eventBatcher.Add("push peerTable")
 
 	node.consensusService = consensusService
+	node.peerService = peerService
 	node.connectionManager = connectionManager
 	node.view = View
 
@@ -89,11 +101,24 @@ func (s *Node) Stream(stream pb.MessageService_StreamServer) (error) {
 		message := &pb.Message{}
 
 		err = proto.Unmarshal(envelope.Payload,message)
+		log.Println("Received Envelop:",envelope)
 	}
 }
 
 func (s *Node) Ping(ctx context.Context, in *pb.Empty) (*pb.Empty, error) {
 	return &pb.Empty{}, nil
+}
+
+func (s *Node) GetPeer(context.Context, *pb.Empty) (*pb.Peer, error){
+
+	peer := &pb.Peer{}
+	peer.Port = s.myInfo.Port
+	peer.PubKey = s.myInfo.PubKey
+	peer.IpAddress = s.myInfo.IpAddress
+	peer.HeartBeat = int32(s.myInfo.HeartBeat)
+	peer.PeerID = s.myInfo.PeerID
+
+	return peer,nil
 }
 
 func (s *Node) listen(){
@@ -105,6 +130,7 @@ func (s *Node) listen(){
 
 	server := grpc.NewServer()
 	pb.RegisterMessageServiceServer(server, s)
+	pb.RegisterPeerServiceServer(server,s)
 	// Register reflection service on gRPC server.
 	reflection.Register(server)
 
@@ -115,8 +141,29 @@ func (s *Node) listen(){
 	}
 }
 
-func main(){
+func (s *Node) RequestPeer(address string) *pb.Peer{
+	log.Println("request peer Information to boot peer:",address)
 
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+	c := pb.NewPeerServiceClient(conn)
+
+	peer, err := c.GetPeer(context.Background(), &pb.Empty{})
+
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	log.Println("recevied peer Info:",peer)
+
+	return peer
+}
+
+func main(){
 
 	app := cli.NewApp()
 
@@ -125,24 +172,46 @@ func main(){
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:        "My Address",
+			Name:        "IP, ip",
 			Usage:       "hostIP:port",
 			Destination: &myAddress,
 		},
 		cli.StringFlag{
-			Name:        "id, I",
+			Name:        "BootIP, bi",
 			Usage:       "hostIP:port",
 			Destination: &bootAddress,
 		},
 	}
 
-	peer := &domain.Peer{}
-	peer.PeerID = "peer1"
-	peer.IpAddress = "localhost"
-	peer.Port = "4444"
+	app.Action = func(c *cli.Context) error {
 
-	node := NewNode(peer)
-	node.listen()
+		address := strings.Split(myAddress,":")
+
+		peer := &domain.Peer{}
+		peer.PeerID = xid.New().String()
+		peer.IpAddress = address[0]
+		peer.Port = address[1]
+
+		log.Println(myAddress)
+
+		node := NewNode(peer)
+
+		if bootAddress != ""{
+			log.Println("searching boot peer...")
+			p := node.RequestPeer(bootAddress)
+			log.Println(p)
+			bootPeer := domain.FromProtoPeer(*p)
+			log.Println(bootPeer)
+			log.Println(node.peerService)
+			node.peerService.AddPeer(bootPeer)
+		}
+
+		node.listen()
+
+		return nil
+	}
+
+	app.Run(os.Args)
 }
 
 
