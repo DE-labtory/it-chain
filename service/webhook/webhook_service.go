32b176b6a56bb2fbbdb99ee53d0095810e48fd8c
yojkim <youthful2016@gmail.com>
2018-02-22 06:43:27 +0900

    add serve method to webhookService

--- service/webhook/webhook_service.go
+++ service/webhook/webhook_service.go
@@ -4,6 +4,8 @@ import "it-chain/domain"
 
 type WebhookService interface {
 
-	SendConfirmedBlock(block *domain.Block) (error)
+	SendConfirmedBlock(block *domain.Block) error
+
+	Serve(port int) error
 
 }
