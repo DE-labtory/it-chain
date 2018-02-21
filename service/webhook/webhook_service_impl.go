5286a20f6ddfede96c4f24b735604f138cbdab8e
yojkim <youthful2016@gmail.com>
2018-02-22 06:44:11 +0900

    add serve implementation and modify part that check validation of requested URL

--- service/webhook/webhook_service_impl.go
+++ service/webhook/webhook_service_impl.go
@@ -13,10 +13,8 @@ import (
 	"google.golang.org/grpc"
 	"net"
 	"log"
-)
-
-const (
-	WEBHOOK_PORT = ":50070"
+	"google.golang.org/grpc/reflection"
+	"fmt"
 )
 
 type WebhookServiceImpl struct {
@@ -39,16 +37,6 @@ func NewWebhookService() (WebhookService, error) {
 		infos: infos,
 	}
 
-	lis, err := net.Listen("tcp", WEBHOOK_PORT)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	grpcServer := grpc.NewServer()
-	pb.RegisterWebhookServer(grpcServer, wi)
-
-	grpcServer.Serve(lis)
-
 	return wi, nil
 
 }
@@ -64,6 +52,8 @@ func (wi *WebhookServiceImpl) Register(ctx context.Context, in *pb.WebhookReques
 	if isExist {
 		return &pb.WebhookResponse{"Already exists"}, errors.New("Already exist")
 	}
+
+	log.Printf("REGISTED URL : %s", parseURL.String())
 
 	wi.infos = append(wi.infos, webhookInfo{*parseURL})
 	return &pb.WebhookResponse{"Success to register your webhook"}, nil
@@ -88,10 +78,43 @@ func (wi *WebhookServiceImpl) Remove(ctx context.Context, in *pb.WebhookRequest)
 
 }
 
+func (wi *WebhookServiceImpl) Serve(port int) error {
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+		return err
+	}
+
+	defer lis.Close()
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterWebhookServer(grpcServer, wi)
+
+	reflection.Register(grpcServer)
+
+	if err = grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to launch grpc server : %v", err)
+		grpcServer.Stop()
+		return err
+	}
+
+	defer grpcServer.Stop()
+
+	return nil
+
+}
+
 func (wi *WebhookServiceImpl) SendConfirmedBlock(block *domain.Block) (error) {
 
 	if block == nil {
 		return errors.New("block should not be nil")
+	}
+
+	if len(wi.infos) == 0 {
+		log.Printf("There is no payload URL")
+		return nil
 	}
 
 	blockBytes, err := json.Marshal(block)
@@ -103,6 +126,7 @@ func (wi *WebhookServiceImpl) SendConfirmedBlock(block *domain.Block) (error) {
 
 	go func() {
 		for _, info := range wi.infos {
+			log.Printf("PAYLOAD URL : %s", info.payloadURL.String())
 			_, err := http.Post(info.payloadURL.String(), "application/json", buff)
 			if err != nil {
 				log.Fatalf("An error during the sending process : %v", err)
@@ -132,7 +156,7 @@ func urlValidCheck(rawURL string) (*url.URL, bool) {
 		return nil, false
 	}
 
-	parseURL, err := url.Parse(rawURL)
+	parseURL, err := url.ParseRequestURI(rawURL)
 	if err != nil {
 		return nil, false
 	}
