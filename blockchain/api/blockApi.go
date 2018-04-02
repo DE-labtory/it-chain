de2bb0a9e2af1596cc8505c1830585c7e9c4c8dc
zerofruit <14wnrkim@gmail.com>
2018-04-03 00:16:16 +0900

    add api/blockApi

---
+++ blockchain/api/blockApi.go
@@ -0,0 +1,15 @@
+package api
+
+import "github.com/it-chain/it-chain-Engine/blockchain/domain/repository"
+
+type BlockApi struct {
+	blockRepository repository.BlockRepository
+}
+
+func (bApi BlockApi) CreateBlock() {
+
+}
+
+func (bApi BlockApi) AddBlock() {
+
+}
