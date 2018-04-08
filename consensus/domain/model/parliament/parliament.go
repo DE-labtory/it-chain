45810ce0260afd30e84258968dc5752dbd1f568b
jun <leebduk@gmail.com>
2018-04-08 15:10:14 +0900

    parliament test case added

--- consensus/domain/model/parliament/parliament.go
+++ consensus/domain/model/parliament/parliament.go
@@ -1,6 +1,10 @@
 package parliament
 
 import (
+	"errors"
+
+	"fmt"
+
 	"github.com/it-chain/it-chain-Engine/consensus/domain/model/consensus"
 )
 
@@ -19,7 +23,15 @@ func NewParliament() *Parliament {
 
 func (p Parliament) IsNeedConsensus() bool {
 
-	if len(p.Members) >= 1 {
+	numOfMember := 0
+
+	if p.HasLeader() {
+		numOfMember = numOfMember + 1
+	}
+
+	numOfMember = numOfMember + len(p.Members)
+
+	if numOfMember >= 1 {
 		return true
 	}
 
@@ -39,8 +51,15 @@ func (p *Parliament) SetLeader(leader *Leader) {
 	p.Leader = leader
 }
 
-func (p *Parliament) AddMember(member *Member) {
+func (p *Parliament) AddMember(member *Member) error {
+
+	if member.ID.ID == "" {
+		return errors.New(fmt.Sprintf("Need Valid PeerID [%s]", member.ID.ID))
+	}
+
 	p.Members = append(p.Members, member)
+
+	return nil
 }
 
 func (p *Parliament) RemoveMember(memberID PeerID) {
@@ -54,7 +73,17 @@ func (p *Parliament) RemoveMember(memberID PeerID) {
 	p.Members = append(p.Members[:index], p.Members[index+1:]...)
 }
 
+//representative가 모두 Paliament에 속해있어야 한다.
 func (p *Parliament) ValidateRepresentative(representatives []*consensus.Representative) bool {
+
+	for _, representatives := range representatives {
+		index := p.findIndexOfMember(PeerID{string(representatives.Id)})
+
+		if index == -1 {
+			return false
+		}
+	}
+
 	return true
 }
 
