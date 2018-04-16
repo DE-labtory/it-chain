9dd2071038fcf9d23c6d21564ec5bf7df141f5e7
park hae sung <hea9549@gmail.com>
2018-04-16 20:33:47 +0900

    txpool domain init

--- txpool/domain/model/transaction/transaction.go
+++ txpool/domain/model/transaction/transaction.go
@@ -1,1 +1,31 @@
 package transaction
+
+import "time"
+
+const (
+	//Tx 상태 Const
+	STATUS_TX_UNCONFIRM TxStatus = iota
+	STATUS_TX_CONFIRM
+	STATUS_TX_UNKNOWN
+
+	//Tx 타입 Const
+	TYPE_DEPLOY TxType = iota
+	TYPE_INVOKE
+	TYPE_QUERY
+
+	HASH_NEED_CALC = "HASH_NEED_CALC"
+)
+
+type TransactionId string
+type TxStatus int
+type TxType int
+
+type Transaction struct {
+	TxId          TransactionId
+	PublishPeerId string
+	TxStatus      TxStatus
+	TxType        TxType
+	TxHash        string
+	TimeStamp     time.Time
+	TxData        *TxData
+}
--- txpool/domain/model/transaction/tx_data.go
+++ txpool/domain/model/transaction/tx_data.go
@@ -1,1 +1,8 @@
 package transaction
+
+type TxData struct {
+	Jsonrpc string
+	Method  string
+	Params  []string
+	ICodeId string `json:"id"`
+}
