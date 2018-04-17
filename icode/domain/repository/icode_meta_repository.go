010ba61e8cde3a54258933477dc3fbd70ea5c574
jun <leebduk@gmail.com>
2018-04-17 14:04:44 +0900

    impl icode meta repo:

--- icode/domain/repository/icode_meta_repository.go
+++ icode/domain/repository/icode_meta_repository.go
@@ -5,6 +5,6 @@ import "github.com/it-chain/it-chain-Engine/icode/domain/model"
 type ICodeMetaRepository interface {
 	Save(iCodeMeta model.ICodeMeta) error
 	Remove(id model.ICodeID) error
-	FindByID(id model.ICodeID) *model.ICodeMeta
-	FindAll() []model.ICodeMeta
+	FindByID(id model.ICodeID) (*model.ICodeMeta, error)
+	FindAll() ([]*model.ICodeMeta, error)
 }
--- icode/infra/repository/leveldb/icode_meta_repository.go
+++ icode/infra/repository/leveldb/icode_meta_repository.go
@@ -1,6 +1,8 @@
 package leveldb
 
 import (
+	"encoding/json"
+
 	"github.com/it-chain/it-chain-Engine/icode/domain/model"
 	"github.com/it-chain/leveldb-wrapper"
 )
@@ -10,6 +12,7 @@ type ICodeMetaRepository struct {
 }
 
 func NewICodeMetaRepository(path string) *ICodeMetaRepository {
+
 	db := leveldbwrapper.CreateNewDB(path)
 
 	return &ICodeMetaRepository{
@@ -18,17 +21,68 @@ func NewICodeMetaRepository(path string) *ICodeMetaRepository {
 }
 
 func (i ICodeMetaRepository) Save(iCodeMeta model.ICodeMeta) error {
+
+	b, err := iCodeMeta.Serialize()
+
+	if err != nil {
+		return err
+	}
+
+	if err = i.leveldb.Put([]byte(iCodeMeta.ID), b, true); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (i ICodeMetaRepository) Remove(id model.ICodeID) error {
-	return nil
+
+	return i.leveldb.Delete([]byte(id), true)
 }
 
-func (i ICodeMetaRepository) FindByID(id model.ICodeID) *model.ICodeMeta {
-	return nil
+func (i ICodeMetaRepository) FindByID(id model.ICodeID) (*model.ICodeMeta, error) {
+
+	b, err := i.leveldb.Get([]byte(id))
+
+	if err != nil {
+		return nil, err
+	}
+
+	var iCodeMeta *model.ICodeMeta
+
+	err = json.Unmarshal(b, iCodeMeta)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return iCodeMeta, nil
 }
 
-func (i ICodeMetaRepository) FindAll() []model.ICodeMeta {
-	return nil
+func (i ICodeMetaRepository) FindAll() ([]*model.ICodeMeta, error) {
+
+	iter := i.leveldb.GetIteratorWithPrefix([]byte(""))
+	iCodeMetas := make([]*model.ICodeMeta, 0)
+
+	for iter.Next() {
+		val := iter.Value()
+		iCodeMeta := &model.ICodeMeta{}
+		err := deserialize(val, iCodeMeta)
+
+		if err != nil {
+			return nil, err
+		}
+
+		iCodeMetas = append(iCodeMetas, iCodeMeta)
+	}
+
+	return iCodeMetas, nil
+}
+
+func deserialize(b []byte, iCodeModel *model.ICodeMeta) error {
+	err := json.Unmarshal(b, iCodeModel)
+
+	if err != nil {
+		return nil
+	}
 }
