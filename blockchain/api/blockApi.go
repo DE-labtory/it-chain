11f130eaab1532581c2b90f37f13c39ffa7f5cc1
zerofruit <14wnrkim@gmail.com>
2018-04-21 14:28:49 +0900

    blockApi AddBlock, GetLastBlock

--- blockchain/api/blockApi.go
+++ blockchain/api/blockApi.go
@@ -1,8 +1,13 @@
 package api
 
-import "github.com/it-chain/it-chain-Engine/blockchain/domain/repository"
+import (
+	"github.com/it-chain/it-chain-Engine/blockchain/domain/model/block"
+	"github.com/it-chain/it-chain-Engine/blockchain/domain/repository"
+	"github.com/it-chain/yggdrasill"
+)
 
 type BlockApi struct {
+	yggDrasill      blockchainleveldb.YggDrasill
 	blockRepository repository.BlockRepository
 }
 
@@ -10,6 +15,10 @@ func (bApi BlockApi) CreateBlock() {
 
 }
 
-func (bApi BlockApi) AddBlock() {
+func (bApi BlockApi) AddBlock(block block.Block) {
+	bApi.yggDrasill.AddBlock(block)
+}
 
+func (bApi BlockApi) GetLastBlock(block block.Block) {
+	bApi.yggDrasill.GetLastBlock(block)
 }
