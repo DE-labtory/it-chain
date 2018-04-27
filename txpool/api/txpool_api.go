446513e433752077f6bbc35234fd37fef6a22530
hihiboss <hihiboss@naver.com>
2018-04-27 15:36:32 +0900

    Define NewTxpoolApi, SaveTransaction function

--- txpool/api/txpool_api.go
+++ txpool/api/txpool_api.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/it-chain/it-chain-Engine/txpool/domain/repository"
 	"github.com/it-chain/it-chain-Engine/txpool/domain/service"
+	"github.com/it-chain/it-chain-Engine/txpool/domain/model/transaction"
 )
 
 // todo api 만들어라 준희야
@@ -13,4 +14,17 @@ type TxpoolApi struct {
 	timeoutTicker *time.Ticker
 	maxTxNum      int
 	messageApi    service.MessageProducer
+}
+
+func NewTxpoolApi () *TxpoolApi{
+	return &TxpoolApi{
+		txRepository:  nil,
+		timeoutTicker: nil,
+		maxTxNum:      0,
+		messageApi:    nil,
+	}
+}
+
+func (txpoolApi TxpoolApi) SaveTransaction(transaction transaction.Transaction) error {
+	return txpoolApi.txRepository.Save(transaction)
 }
