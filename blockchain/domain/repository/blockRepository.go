41372bb7736fc0dcf347bccacea604141c9ac6f2
zerofruit <14wnrkim@gmail.com>
2018-04-29 01:39:21 +0900

    change blockRepository return type

--- blockchain/domain/repository/blockRepository.go
+++ blockchain/domain/repository/blockRepository.go
@@ -2,11 +2,12 @@ package repository
 
 import (
 	"github.com/it-chain/it-chain-Engine/blockchain/domain/model/block"
+	"github.com/it-chain/yggdrasill/transaction"
 )
 
 type BlockRepository interface {
 	Close()
 	AddBlock(block block.Block) error
-	GetLastBlock(block block.Block)
-	GetTransactionsById(id int) // block과 관련된 정보 조회 예시
+	GetLastBlock() block.Block
+	GetTransactionsById(id string) transaction.Transaction
 }
