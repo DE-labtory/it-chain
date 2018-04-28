83f927e5228e363f46000b70076a2b1ff780a0ad
hihiboss <hihiboss@naver.com>
2018-04-28 11:31:23 +0900

    Edit api/txpool_api.go for using timeoutTicker model and getting txpool configuration

--- txpool/api/txpool_api.go
+++ txpool/api/txpool_api.go
@@ -1,33 +1,34 @@
 package api
 
 import (
-	"time"
-
 	"github.com/it-chain/it-chain-Engine/txpool/domain/repository"
 	"github.com/it-chain/it-chain-Engine/txpool/domain/service"
 	"github.com/it-chain/it-chain-Engine/txpool/domain/model/transaction"
+	"github.com/it-chain/it-chain-Engine/txpool/domain/model/timeout"
 	"github.com/it-chain/it-chain-Engine/conf"
 )
 
 // todo api 만들어라 준희야
 type TxpoolApi struct {
 	txRepository  repository.TransactionRepository
-	timeoutTicker *time.Ticker
-	maxTxNum      int
+	timeoutTicker *timeout.TimeoutTicker
+	maxTxByte     int
 	messageApi    service.MessageProducer
 }
 
+// TODO assign txRepo, msgApi
 func NewTxpoolApi () *TxpoolApi{
-	txpoolConfig = conf.GetConfiguration().Txpool
+	txpConfig := conf.GetConfiguration().Txpool
 
 	return &TxpoolApi{
 		txRepository:  nil,
-		timeoutTicker: nil,
-		maxTxNum:      0,
+		timeoutTicker: timeout.NewTimeoutTicker(txpConfig.TimoutMs),
+		maxTxByte:     txpConfig.MaxTransactionByte,
 		messageApi:    nil,
 	}
 }
 
+// TODO impl
 func (txpoolApi TxpoolApi) SaveTransaction(transaction transaction.Transaction) error {
 	return txpoolApi.txRepository.Save(transaction)
 }
