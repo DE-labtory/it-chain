88efd37ac636010f53ee7e5d38606ec0bbf98623
junk-sound <wom2277@naver.com>
2018-05-01 16:22:10 +0900

    Add ToDo

--- blockchain/domain/service/block.go
+++ blockchain/domain/service/block.go
@@ -12,6 +12,7 @@ func CreateGenesisBlock(genesisconfFilePath string) (*block.DefaultBlock, error)
 	if err != nil {
 		return nil, err
 	}
+	//ToDo: yggdrasill의 Update사항을 반영하여, validate과정을 추가하여야 합니다.(주석 작성자 GitID:junk-sound)
 
 	var GenesisBlock *block.DefaultBlock
 	json.Unmarshal(byteValue, &GenesisBlock)
