16a65a3317fd137ddd8f9ad5e347f2a19ac9fdd9
jun <leebduk@gmail.com>
2018-05-10 22:36:39 +0900

    cli added and cmd pkg added

---
+++ cmd/peer/start.go
@@ -0,0 +1,26 @@
+package peer
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli"
+)
+
+func StartCmd() cli.Command {
+
+	return cli.Command{
+		Name:    "start",
+		Aliases: []string{"s"},
+		Usage:   "start peer as background",
+		Action: func(c *cli.Context) error {
+			fmt.Println("peer is starting...")
+			start()
+			return nil
+		},
+	}
+}
+
+//start peer
+func start() {
+
+}
