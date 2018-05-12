83ea7f89854e90e89169a8ddbbeb345cafcf6e06
Park Hae Sung <hea9549@gmail.com>
2018-05-12 20:33:56 +0900

    peer 관련 이벤트 추가

--- messaging/rabbitmq/event/peer_event.go
+++ messaging/rabbitmq/event/peer_event.go
@@ -2,12 +2,26 @@ package event
 
 //todo define event
 type LeaderChangeEvent struct {
-}
-
-//todo define event
-type PeerConnectEvent struct {
+	PeerId string
 }
 
 //todo define event
 type PeerDisconnectEvent struct {
+	PeerId string
+}
+
+//todo define event
+type PeerConnectEvent struct {
+	PeerId string
+}
+
+//todo define event
+type LeaderInfoDeliverCmd struct {
+	timeUnix int64
+}
+
+//todo define event
+type LeaderInfoReceiveEvent struct {
+	PeerId  string
+	Address string
 }
