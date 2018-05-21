a81793b6fe4456374c61cb59e3accd1286991290
junk-sound <wom2277@naver.com>
2018-05-21 15:43:14 +0900

    changed block.go/CreateGenesisBlock

--- blockchain/domain/service/block.go
+++ blockchain/domain/service/block.go
@@ -2,18 +2,40 @@ package service
 
 import (
 	"encoding/json"
+
 	"time"
+
+	"fmt"
 
 	"github.com/it-chain/yggdrasill/impl"
 )
 
 func CreateGenesisBlock(genesisconfFilePath string) (*impl.DefaultBlock, error) {
+	fmt.Println("Service/block.go")
 	byteValue, err := ConfigFromJson(genesisconfFilePath)
 	if err != nil {
 		return nil, err
 	}
+
+	validator := new(impl.DefaultValidator)
+
 	var GenesisBlock *impl.DefaultBlock
+
 	json.Unmarshal(byteValue, &GenesisBlock)
-	GenesisBlock.Timestamp = time.Now()
+	GenesisBlock.SetTimestamp((time.Now()))
+	Seal, err := validator.BuildSeal(GenesisBlock)
+	if err != nil {
+		return nil, err
+	}
+
+	GenesisBlock.SetSeal(Seal)
+	fmt.Println("Seal:")
+	fmt.Println(Seal)
+
+	GenesisBlock.SetPrevSeal(GenesisBlock.PrevSeal)
+	GenesisBlock.SetHeight(GenesisBlock.Height)
+	GenesisBlock.SetHeight(GenesisBlock.Height)
+	GenesisBlock.SetTxSeal(GenesisBlock.TxSeal)
+	GenesisBlock.SetCreator(GenesisBlock.Creator)
 	return GenesisBlock, nil
 }
