6aba098e28cd3e309e1585c5266d12f4afcbee89
junk-sound <wom2277@naver.com>
2018-05-21 16:39:08 +0900

    delete trash

--- blockchain/domain/service/block.go
+++ blockchain/domain/service/block.go
@@ -5,13 +5,10 @@ import (
 
 	"time"
 
-	"fmt"
-
 	"github.com/it-chain/yggdrasill/impl"
 )
 
 func CreateGenesisBlock(genesisconfFilePath string) (*impl.DefaultBlock, error) {
-	fmt.Println("Service/block.go")
 	byteValue, err := ConfigFromJson(genesisconfFilePath)
 	if err != nil {
 		return nil, err
@@ -22,16 +19,13 @@ func CreateGenesisBlock(genesisconfFilePath string) (*impl.DefaultBlock, error) 
 	var GenesisBlock *impl.DefaultBlock
 
 	json.Unmarshal(byteValue, &GenesisBlock)
-	GenesisBlock.SetTimestamp((time.Now()))
+	GenesisBlock.SetTimestamp((time.Now()).Round(0))
 	Seal, err := validator.BuildSeal(GenesisBlock)
 	if err != nil {
 		return nil, err
 	}
 
 	GenesisBlock.SetSeal(Seal)
-	fmt.Println("Seal:")
-	fmt.Println(Seal)
-
 	GenesisBlock.SetPrevSeal(GenesisBlock.PrevSeal)
 	GenesisBlock.SetHeight(GenesisBlock.Height)
 	GenesisBlock.SetHeight(GenesisBlock.Height)
