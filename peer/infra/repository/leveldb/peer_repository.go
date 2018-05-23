b63a3ed2f083ec7340fd7d1ec4b2da2d1b15c528
frontalnh <frontalnh@gmail.com>
2018-05-23 15:37:37 +0900

    peer repo comment added

--- peer/infra/repository/leveldb/peer_repository.go
+++ peer/infra/repository/leveldb/peer_repository.go
@@ -13,6 +13,8 @@ type PeerRepository struct {
 }
 
 // 새로운 peer repo 생성
+// 원래 코드가 임의 코드인 것 같아서 leveldb wrapper usage 참고하여 수정하였습니다.
+// leveldb에 적합한 db를 제공하여 기존의 다른 함수들은 잘 동작할 것으로 예상되며 leveldbwrapper 참고해주시면 될 것 같습니다
 func NewPeerRepository(path string) *PeerRepository {
 	// path := "./leveldb"
 	dbProvider := leveldbwrapper.CreateNewDBProvider(path)
