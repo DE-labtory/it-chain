050212e7f9a5aaafbd13f21356a71e916a380932
KimJunHui <junhui820@gmail.com>
2018-05-28 18:25:14 +0900

    Impl TransferTxToLeader in transfer_service.go

--- txpool/infra/timeout_service.go
+++ txpool/infra/timeout_service.go
@@ -1,6 +1,10 @@
 package infra
 
-import "time"
+import (
+	"time"
+
+	"github.com/it-chain/it-chain-Engine/txpool"
+)
 
 type TimeoutTicker struct {
 	T         *time.Ticker
@@ -9,10 +13,10 @@ type TimeoutTicker struct {
 
 func NewTimeoutTicker(timeoutMs int64) *TimeoutTicker {
 	tTicker := time.NewTicker(time.Duration(timeoutMs))
+	tTicker.Stop()
+
 	// Duration represents the time as int64 nanosecond count.
 	tMs := time.Duration(timeoutMs * 100000)
-
-	tTicker.Stop()
 
 	return &TimeoutTicker{
 		T:         tTicker,
@@ -22,6 +26,12 @@ func NewTimeoutTicker(timeoutMs int64) *TimeoutTicker {
 
 func (t *TimeoutTicker) Start() {
 	t.T = time.NewTicker(t.TimeoutMs)
+	go func() {
+		for _ := range t.T.C {
+			// todo : Run "TransferTxToLeader()"
+			txpool.TxPeriodicTransferService.TransferTxToLeader()
+		}
+	}()
 }
 
 func (t *TimeoutTicker) Stop() {
--- txpool/transfer_service.go
+++ txpool/transfer_service.go
@@ -1,5 +1,10 @@
 package txpool
 
+import (
+	"log"
+	"errors"
+)
+
 //todo
 type TxPeriodicTransferService struct {
 	txRepository      TransactionRepository
@@ -10,5 +15,22 @@ type TxPeriodicTransferService struct {
 //todo 이 함수가 call되었을 때 조건에 맞는 tx를 leader에게 전송하는 로직 추가
 //todo infra의 timeout_service에 이 함수를 등록, timeout_service가 시간단위로 이 함수를 실행
 func (t TxPeriodicTransferService) TransferTxToLeader() {
+	// todo : 조건에 맞는 tx가 어떤게 있을까...
+	transactions, err := t.txRepository.FindAll()
 
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	leader := t.leaderRepository.GetLeader()
+
+	if leader.StringLeaderId() == "" {
+		log.Println(errors.New("there is no leader"))
+	}
+
+	err = t.messageDispatcher.SendTransactions(transactions, leader)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
 }
