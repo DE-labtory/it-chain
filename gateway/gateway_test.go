c15119a37dbdce6c8e7619dc7950471dcf6db252
jun <leebduk@gmail.com>
2018-05-31 22:14:01 +0900

    delete conf additional file and add test case

--- gateway/gateway_test.go
+++ gateway/gateway_test.go
@@ -5,9 +5,7 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/it-chain/bifrost"
 	"github.com/it-chain/it-chain-Engine/gateway"
-	"github.com/it-chain/midgard/bus/rabbitmq"
 )
 
 func TestStart(t *testing.T) {
@@ -26,64 +24,6 @@ func TestStart(t *testing.T) {
 		os.RemoveAll("~test2")
 		gateway.Stop()
 	}()
-
-	wg.Wait()
-}
-
-func TestStart2(t *testing.T) {
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
-	amqpUrl := "amqp://guest:guest@localhost:5672/"
-	keyPath := "~test3"
-	rabbitmqClient := rabbitmq.Connect(amqpUrl)
-
-	defer func() {
-		os.RemoveAll(keyPath)
-		rabbitmqClient.Close()
-	}()
-
-	//create connection store by bifrost which is it-chain's own lib for implementing p2p network
-	ConnectionStore := bifrost.NewConnectionStore()
-
-	pri, pub := gateway.LoadKeyPair(keyPath)
-	//load key
-
-	//createHandler
-	connectionHandler := gateway.NewConnectionCommandHandler(ConnectionStore, pri, pub, rabbitmqClient)
-	connectionHandler.HandleConnectionCreate(gateway.ConnectionCreateCommand{
-		Address: "127.0.0.1:7777",
-	})
-
-	wg.Wait()
-}
-
-func TestStart3(t *testing.T) {
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
-	amqpUrl := "amqp://guest:guest@localhost:5672/"
-	keyPath := "~test4"
-	rabbitmqClient := rabbitmq.Connect(amqpUrl)
-
-	defer func() {
-		os.RemoveAll(keyPath)
-		rabbitmqClient.Close()
-	}()
-
-	//create connection store by bifrost which is it-chain's own lib for implementing p2p network
-	ConnectionStore := bifrost.NewConnectionStore()
-
-	pri, pub := gateway.LoadKeyPair(keyPath)
-	//load key
-
-	//createHandler
-	connectionHandler := gateway.NewConnectionCommandHandler(ConnectionStore, pri, pub, rabbitmqClient)
-	connectionHandler.HandleConnectionCreate(gateway.ConnectionCreateCommand{
-		Address: "127.0.0.1:7777",
-	})
 
 	wg.Wait()
 }
