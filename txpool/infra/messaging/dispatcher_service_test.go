2e4e2b7438cef00f938f9bcaafab8d9c494f60f7
zerofruit <14wnrkim@gmail.com>
2018-06-04 13:54:37 +0900

    SendLeaderTransactions

--- txpool/infra/messaging/dispatcher_service_test.go
+++ txpool/infra/messaging/dispatcher_service_test.go
@@ -7,9 +7,56 @@ import (
 	"errors"
 )
 
-func Test
+func TestMessageDispatcher_SendLeaderTransactions_TransactionsEmpty(t *testing.T) {
+	// Given
+	publisher := Publisher(func(exchange string, topic string, data interface{}) error {
+		return nil
+	})
+	md := MessageDispatcher{
+		publisher: publisher,
+	}
+	transactions := []*txpool.Transaction{}
+	leader := txpool.Leader{
+		LeaderId: txpool.LeaderId{
+			Id: "888",
+		},
+	}
 
-func TestMessageDispatcher_ProposeBlock_TransactionEmpty(t *testing.T) {
+	// When
+	err := md.SendLeaderTransactions(transactions, leader)
+
+	// Then
+	assert.Equal(t, errors.New("Empty transaction list proposed"), err)
+
+}
+
+func TestMessageDispatcher_SendLeaderTransactions(t *testing.T) {
+	// Given
+	publisher := Publisher(func(exchange string, topic string, data interface{}) error {
+		return nil
+	})
+	md := MessageDispatcher{
+		publisher: publisher,
+	}
+	transactions := []*txpool.Transaction{}
+	leader := txpool.Leader{
+		LeaderId: txpool.LeaderId{
+			Id: "888",
+		},
+	}
+
+	// When
+	t1 := txpool.Transaction{
+		TxId: "111",
+	}
+	transactions = append(transactions, &t1)
+	err := md.SendLeaderTransactions(transactions, leader)
+
+	// Then
+	assert.Equal(t, nil, err);
+}
+
+func TestMessageDispatcher_ProposeBlock_TransactionsEmpty(t *testing.T) {
 	// Given
 	publisher := Publisher(func(exchange string, topic string, data interface{}) error {
 		return nil
