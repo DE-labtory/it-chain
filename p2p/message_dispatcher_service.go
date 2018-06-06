5758c2dddaba98513d15543430ad897f9a78b02b
frontalnh <frontalnh@gmail.com>
2018-06-06 21:48:24 +0900

    fix error

--- p2p/message_dispatcher_service.go
+++ p2p/message_dispatcher_service.go
@@ -5,6 +5,6 @@ type MessageDispatcher interface {
 	DeliverLeaderInfo(toPeer Node, leader Node) error
 	RequestTable(toNode Node) error
 	ResponseTable(toNode Node, nodes []Node) error
-	LeaderUpdateEvent(leader Node) error
-	DeliverNodeList(toNode Node) error // update node repository in specific node
+	SendLeaderUpdateMessage(leader Node) error
+	SendDeliverNodeListMessage(toNode Node) error // update node repository in specific node
 }
