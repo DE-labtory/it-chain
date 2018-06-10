b6d7e33e33d67e7da2e587c74600991fcc503016
jun <leebduk@gmail.com>
2018-06-10 14:30:34 +0900

    refactored message dispatcher

--- p2p/infra/messaging/message_dispatcher.go
+++ p2p/infra/messaging/message_dispatcher.go
@@ -21,77 +21,61 @@ func NewMessageDispatcher(publisher midgard.Publisher) *MessageDispatcher {
 }
 
 // publish command to amqp to get leader info from other node
-func (md *MessageDispatcher) RequestLeaderInfo(peer p2p.Node) error {
+func (md *MessageDispatcher) RequestLeaderInfo(nodeId p2p.NodeId) error {
 
-	requestBody := p2p.LeaderInfoRequestMessage{
+	body := p2p.LeaderInfoRequestMessage{
 		TimeUnix: time.Now().Unix(),
 	}
 
-	requestBodyByte, _ := common.Serialize(requestBody)
+	deliverCommand, err := CreateMessageDeliverCommand(event.LeaderInfoDeliverProtocol, body)
 
-	deliverCom := &p2p.MessageDeliverCommand{
-		CommandModel: midgard.CommandModel{
-			ID: xid.New().String(),
-		},
-		Recipients: make([]string, 0),
-		Body:       requestBodyByte,
-		Protocol:   "LeaderInfoRequestMessage",
+	if err != nil {
+		return err
 	}
-	deliverCom.Recipients = append(deliverCom.Recipients, peer.NodeId.ToString())
 
-	return md.publisher.Publish("Command", "GrpcMessage", deliverCom)
+	deliverCommand.Recipients = append(deliverCommand.Recipients, nodeId.ToString())
+
+	return md.publisher.Publish("Command", "message.deliver", deliverCommand)
 }
 
 // 단일 피어에게 새로운 리더 정보를 전달하는 메서드이다.
-func (md *MessageDispatcher) DeliverLeaderInfo(toPeer p2p.Node, leader p2p.Node) error {
+func (md *MessageDispatcher) DeliverLeaderInfo(nodeId p2p.NodeId, leader p2p.Node) error {
 
 	// 리더 정보를 leaderInfoBody에 담아줌
-	leaderInfoBody := p2p.LeaderInfoResponseMessage{
+	body := p2p.LeaderInfoResponseMessage{
 		LeaderId: leader.NodeId.ToString(),
 		Address:  leader.IpAddress,
 	}
 
-	// 리더 정보 json byte 변환
-	leaderInfoBodyByte, _ := common.Serialize(leaderInfoBody)
+	deliverCommand, err := CreateMessageDeliverCommand(event.LeaderInfoDeliverProtocol, body)
 
-	// 메세지 전달 이벤트 구조를 담는다.
-	deliverCommand := p2p.MessageDeliverCommand{
-		CommandModel: midgard.CommandModel{
-			ID: xid.New().String(),
-		},
-		Recipients: make([]string, 0),
-		Body:       leaderInfoBodyByte,
-		Protocol:   event.LeaderInfoDeliverProtocol,
+	if err != nil {
+		return err
 	}
 
 	// 메세지를 수신할 수신자들을 지정해 준다.
-	deliverCommand.Recipients = append(deliverCommand.Recipients, toPeer.NodeId.ToString())
+	deliverCommand.Recipients = append(deliverCommand.Recipients, nodeId.ToString())
 
 	// topic 과 serilized data를 받아 publisher 한다.
-	return md.publisher.Publish("Command", "MessageDeliverCommand", deliverCommand)
+	return md.publisher.Publish("Command", "message.deliver", deliverCommand)
 }
 
 // command message which requests node list of specific node
-func (md *MessageDispatcher) RequestNodeList(peer p2p.Node) error {
+func (md *MessageDispatcher) RequestNodeList(nodeId p2p.NodeId) error {
 
-	requestBody := p2p.LeaderInfoRequestMessage{
+	body := p2p.LeaderInfoRequestMessage{
 		TimeUnix: time.Now().Unix(),
 	}
 
-	requestBodyByte, _ := common.Serialize(requestBody)
+	deliverCommand, err := CreateMessageDeliverCommand("NodeListRequestMessage", body)
 
-	commandMessage := &p2p.MessageDeliverCommand{
-		CommandModel: midgard.CommandModel{
-			ID: xid.New().String(),
-		},
-		Recipients: make([]string, 0),
-		Body:       requestBodyByte,
-		Protocol:   "NodeListRequestMessage",
+	if err != nil {
+		return err
 	}
 
-	commandMessage.Recipients = append(commandMessage.Recipients, peer.NodeId.ToString())
+	deliverCommand.Recipients = append(deliverCommand.Recipients, nodeId.ToString())
 
-	return md.publisher.Publish("Commnand", "GrpcMessage", commandMessage)
+	return md.publisher.Publish("Commnand", "message.deliver", deliverCommand)
 }
 
 func (md *MessageDispatcher) ResponseTable(toNode p2p.Node, nodes []p2p.Node) error {
@@ -103,34 +87,45 @@ func (md *MessageDispatcher) ResponseTable(toNode p2p.Node, nodes []p2p.Node) er
 //todo fix path
 func (md *MessageDispatcher) SendLeaderUpdateMessage(nodeId p2p.NodeId, leader p2p.Node) error {
 
-	leaderByte, _ := common.Serialize(leader)
-	deliverCommand := p2p.MessageDeliverCommand{
-		CommandModel: midgard.CommandModel{
-			ID: xid.New().String(),
-		},
-		Recipients: make([]string, 0),
-		Body:       leaderByte,
-		Protocol:   "UpdateLeader",
+	deliverCommand, err := CreateMessageDeliverCommand("UpdateLeader", leader)
+
+	if err != nil {
+		return err
 	}
+
 	deliverCommand.Recipients = append(deliverCommand.Recipients, nodeId.ToString())
 
-	return md.publisher.Publish("Command", "message.*", deliverCommand)
+	return md.publisher.Publish("Command", "message.deliver", deliverCommand)
 }
 
 // deliver content of node repository to new node
-func (md *MessageDispatcher) SendDeliverNodeListMessage(nodeId p2p.NodeId, nodeList []p2p.Node) error{
-	nodeListByte, _ := common.Serialize(nodeList)
+func (md *MessageDispatcher) SendDeliverNodeListMessage(nodeId p2p.NodeId, nodeList []p2p.Node) error {
 
-	deliverCommand := p2p.MessageDeliverCommand{
+	deliverCommand, err := CreateMessageDeliverCommand("NodeListDeliver", nodeList)
+
+	if err != nil {
+		return err
+	}
+
+	deliverCommand.Recipients = append(deliverCommand.Recipients, nodeId.ToString())
+
+	return md.publisher.Publish("Command", "message.deliver", deliverCommand)
+}
+
+func CreateMessageDeliverCommand(protocol string, body interface{}) (p2p.MessageDeliverCommand, error) {
+
+	data, err := common.Serialize(body)
+
+	if err != nil {
+		return p2p.MessageDeliverCommand{}, err
+	}
+
+	return p2p.MessageDeliverCommand{
 		CommandModel: midgard.CommandModel{
 			ID: xid.New().String(),
 		},
 		Recipients: make([]string, 0),
-		Body:       nodeListByte,
-		Protocol:   "NodeListDeliver",
-	}
-	deliverCommand.Recipients = append(deliverCommand.Recipients, nodeId.ToString())
-
-	return md.publisher.Publish("Command", "message.*", deliverCommand)
+		Body:       data,
+		Protocol:   protocol,
+	}, err
 }
-
