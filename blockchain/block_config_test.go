0ef3a67fb216250e5c7a467592108a27cbdb7755
junk-sound <wom2277@naver.com>
2018-06-18 16:59:45 +0900

    junksound: modify words

--- blockchain/block_config_test.go
+++ blockchain/block_config_test.go
@@ -26,9 +26,9 @@ func TestConfigFromJson(t *testing.T) {
 								  "TimeStamp":"0001-01-01T00:00:00-00:00",
 								  "Creator":[]
 								}`)
-	var tempBlock impl.DefaultBlock
-	_ = json.Unmarshal(GenesisBlockConfigJson, &tempBlock)
-	GenesisBlockConfigByte, _ := json.Marshal(tempBlock)
+	var tempJson impl.DefaultBlock
+	_ = json.Unmarshal(GenesisBlockConfigJson, &tempJson)
+	GenesisBlockConfigByte, _ := json.Marshal(tempJson)
 	_ = ioutil.WriteFile(genesisFilePath, GenesisBlockConfigByte, 0644)
 	_, err1 := ConfigFromJson(genesisFilePath)
 	assert.NoError(t, err1)
