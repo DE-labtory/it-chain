cea3af04f4a99feac9cc352c0d763c01d808c180
junk-sound <wom2277@naver.com>
2018-06-20 22:51:41 +0900

    set for blocksync/message-service

--- blockchain/infra/adapter/message_service.go
+++ blockchain/infra/adapter/message_service.go
@@ -1,2 +1,39 @@
 package adapter
 
+import (
+	"errors"
+
+	"github.com/it-chain/it-chain-Engine/p2p"
+)
+
+//kind of error
+var ErrEmptyNodeId = errors.New("empty nodeid proposed")
+
+type Publish func(exchange string, topic string, data interface{}) (err error) // 나중에 의존성 주입을 해준다.
+
+type MessageService struct {
+	publish Publish // midgard.client.Publish
+}
+
+func NewMessageService(publish Publish) *MessageService {
+	return &MessageService{
+		publish: publish,
+	}
+}
+
+//request leader information in p2p network to the node specified by nodeId
+func (md *MessageService) RequestBlock(nodeId p2p.NodeId) error {
+	/*
+		입력값세팅:
+		바디세팅:
+		커맨드세팅:
+		퍼블리쉬:
+	*/
+
+	return md.publish()
+}
+
+func (md *MessageService) ResponseBlock(nodeId p2p.NodeId) error {
+
+	return md.publish()
+}
