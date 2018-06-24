475509d5884caadfd3abfb882fc46a53ccc260ce
zerofruit <14wnrkim@gmail.com>
2018-06-24 23:57:33 +0900

    remove handleProposeBlockCommand

--- blockchain/infra/adapter/command_handler.go
+++ blockchain/infra/adapter/command_handler.go
@@ -3,7 +3,6 @@ package adapter
 import (
 	"github.com/it-chain/it-chain-Engine/blockchain/api"
 	"github.com/it-chain/it-chain-Engine/blockchain"
-	"github.com/it-chain/it-chain-Engine/txpool"
 )
 
 type BlockchainCommandHandler struct {
@@ -21,29 +20,5 @@ func NewBlockchainCommandHandler(blockApi api.BlockApi, nodeApi api.NodeApi) *Bl
 // todo
 func (b *BlockchainCommandHandler) HandleUpdateNodesCommand(command blockchain.NodeUpdateCommand) {
 	panic("implement me")
-}
-
-
-func (handler *BlockchainCommandHandler) HandleProposeBlockCommand(cmd blockchain.ProposeBlockCommand, dispatcher blockchain.MessageDispatcher) {
-	rawTxList := cmd.Transactions
-
-	txList, err := convertTxList(rawTxList)
-	if err != nil {
-		// TODO: handle errors
-		return
-	}
-
-	block, err := handler.blockApi.CreateBlock(txList)
-	if err != nil {
-		// TODO: handle errors
-		return
-	}
-
-	dispatcher.SendBlockValidateCommand(block)
-}
-
-// TODO: yggdrasill/impl/Transaction과 txpool/Transaction이 다름.
-func convertTxList(txList []txpool.Transaction) ([]blockchain.Transaction, error) {
-	return nil, nil
 }
 
