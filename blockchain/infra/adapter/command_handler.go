15c1a85e988c446a7a5631b59a46de5d9cc24f19
zerofruit <14wnrkim@gmail.com>
2018-06-26 01:16:19 +0900

    node api interface

--- blockchain/infra/adapter/command_handler.go
+++ blockchain/infra/adapter/command_handler.go
@@ -5,38 +5,59 @@ import (
 	"github.com/it-chain/it-chain-Engine/blockchain"
 	"github.com/it-chain/it-chain-Engine/txpool"
 	"log"
+	"errors"
 )
 
-type BlockchainCommandHandler struct {
-	nodeApi api.NodeApi
+var ErrEmptyEventId = errors.New("empty event id proposed.")
+var ErrEmptyIpAddress = errors.New("empty ip address proposed.")
+var ErrEmptyNodeId = errors.New("empty node id proposed.")
+var ErrNodeApi = errors.New("problem in node api")
+
+type CommandHandlerNodeApi interface {
+	UpdateNode(node blockchain.Node) error
+}
+
+type CommandHandler struct {
+	nodeApi CommandHandlerNodeApi
 	blockApi api.BlockApi
 }
 
-func NewBlockchainCommandHandler(blockApi api.BlockApi, nodeApi api.NodeApi) *BlockchainCommandHandler {
-	return &BlockchainCommandHandler{
+func NewBlockchainCommandHandler(blockApi api.BlockApi, nodeApi CommandHandlerNodeApi) *CommandHandler {
+	return &CommandHandler{
 		nodeApi: nodeApi,
 		blockApi: blockApi,
 	}
 }
 
-// todo
-func (b *BlockchainCommandHandler) HandleUpdateNodeCommand(command blockchain.NodeUpdateCommand) {
-	nodeID := command.GetID()
+/// 임의로 선정한 노드의 정보를 업데이트한다.
+func (b *CommandHandler) HandleUpdateNodeCommand(command blockchain.NodeUpdateCommand) {
+	eventID := command.GetID()
 
-	if nodeID == "" {
-		log.Println("node id is empty")
+	if eventID == "" {
+		log.Fatal(ErrEmptyEventId)
 		return
 	}
 
 	node := command.Node
+
+	if node.IpAddress == "" {
+		log.Fatal(ErrEmptyIpAddress)
+		return
+	}
+
+	if node.NodeId.Id == "" {
+		log.Fatal(ErrEmptyNodeId)
+		return
+	}
+
 	err := b.nodeApi.UpdateNode(node)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("%s: %s", ErrNodeApi, err)
 	}
 }
 
-func (handler *BlockchainCommandHandler) HandleProposeBlockCommand(command blockchain.ProposeBlockCommand) {
+func (handler *CommandHandler) HandleProposeBlockCommand(command blockchain.ProposeBlockCommand) {
 	//rawTxList := command.Transactions
 	//
 	//txList, err := convertTxList(rawTxList)
--- blockchain/infra/adapter/command_handler_test.go
+++ blockchain/infra/adapter/command_handler_test.go
@@ -1,1 +1,40 @@
-package adapter
+package adapter_test
+
+import (
+	"testing"
+	"github.com/it-chain/it-chain-Engine/blockchain"
+	"fmt"
+)
+
+type CommandHandlerMockNodeApi struct {}
+func (na *CommandHandlerMockNodeApi) UpdateNode(node blockchain.Node) error {return nil}
+
+func TestBlockchainCommandHandler_HandleUpdateNodeCommand(t *testing.T) {
+	tests := map[string] struct {
+		input struct {
+			ID string
+			nodeId string
+			address string
+		}
+		err error
+	}{
+		"success": {
+			input: struct {
+				ID string
+				nodeId string
+				address string
+			}{ID: string("zf"), nodeId: string("1"), address: string("11.22.33.44")},
+			err: nil,
+		},
+		"empty eventId test": {
+			input: struct {
+				ID string
+				nodeId string
+				address string
+			}{ID: string("zf"), nodeId: string(""), address: string("11.22.33.44")}
+		},
+	}
+	fmt.Println(tests)
+}
+
+
