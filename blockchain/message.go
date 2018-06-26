6d70c364b122e15b5b3a41064f878f4afe69b245
junk-sound <wom2277@naver.com>
2018-06-26 22:16:53 +0900

    delete BlockResponseMsg

--- blockchain/message.go
+++ blockchain/message.go
@@ -1,11 +1,5 @@
 package blockchain
 
-import "github.com/it-chain/yggdrasill/impl"
-
 type BlockRequestMessage struct {
 	Height uint64
-}
-
-type BlockResponseMessage struct {
-	Block *impl.DefaultBlock
 }
