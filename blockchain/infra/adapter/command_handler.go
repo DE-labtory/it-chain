913b5e36558cf70501c1417c4cfe7fe2fb260c87
zerofruit <14wnrkim@gmail.com>
2018-06-26 23:53:16 +0900

    delete command_handler HandleUpdateNodeCommand

--- blockchain/infra/adapter/command_handler.go
+++ blockchain/infra/adapter/command_handler.go
@@ -5,53 +5,19 @@ import (
 	"github.com/it-chain/it-chain-Engine/txpool"
 )
 
-type CommandHandlerNodeApi interface {
-	UpdateNode(node blockchain.Node) error
-}
-
 type CommandHandlerBlockApi interface {
 	CreateGenesisBlock(genesisConfFilePath string) (blockchain.Block, error)
 	CreateBlock(txList []blockchain.Transaction) (blockchain.Block, error)
 }
 
 type CommandHandler struct {
-	nodeApi CommandHandlerNodeApi
 	blockApi CommandHandlerBlockApi
 }
 
-func NewCommandHandler(blockApi CommandHandlerBlockApi, nodeApi CommandHandlerNodeApi) *CommandHandler {
+func NewCommandHandler(blockApi CommandHandlerBlockApi) *CommandHandler {
 	return &CommandHandler{
-		nodeApi: nodeApi,
 		blockApi: blockApi,
 	}
-}
-
-/// 임의로 선정한 노드의 정보를 업데이트한다.
-func (b *CommandHandler) HandleUpdateNodeCommand(command blockchain.NodeUpdateCommand) {
-	//eventID := command.GetID()
-	//
-	//if eventID == "" {
-	//	log.Println(ErrEmptyEventId)
-	//	return
-	//}
-	//
-	//node := command.Node
-	//
-	//if node.IpAddress == "" {
-	//	log.Println(ErrEmptyIpAddress)
-	//	return
-	//}
-	//
-	//if node.NodeId.Id == "" {
-	//	log.Println(ErrEmptyNodeId)
-	//	return
-	//}
-	//
-	//err := b.nodeApi.UpdateNode(node)
-	//
-	//if err != nil {
-	//	log.Println("%s: %s", ErrNodeApi, err)
-	//}
 }
 
 func (handler *CommandHandler) HandleProposeBlockCommand(command blockchain.ProposeBlockCommand) {
