5a43ca965216d62f86fbf14ee15f59b887d50b17
frontalnh <frontalnh@gmail.com>
2018-06-27 12:04:20 +0900

    last fix...

--- p2p/infra/adapter/grpc_command_handler_test.go
+++ p2p/infra/adapter/grpc_command_handler_test.go
@@ -13,22 +13,22 @@ import (
 type MockLeaderApi struct{}
 
 func (mla MockLeaderApi) UpdateLeader(leader p2p.Leader) error { return nil }
-func (mla MockLeaderApi) DeliverLeaderInfo(nodeId p2p.NodeId)  {}
+func (mla MockLeaderApi) DeliverLeaderInfo(nodeId p2p.PeerId)  {}
 
-type MockNodeApi struct{}
+type MockPeerApi struct{}
 
-func (mna MockNodeApi) UpdateNodeList(nodeList []p2p.Node) error { return nil }
-func (mna MockNodeApi) DeliverNodeList(nodeId p2p.NodeId) error  { return nil }
-func (mna MockNodeApi) AddNode(node p2p.Node)                    {}
+func (mna MockPeerApi) UpdatePeerList(nodeList []p2p.Peer) error { return nil }
+func (mna MockPeerApi) DeliverPeerList(nodeId p2p.PeerId) error  { return nil }
+func (mna MockPeerApi) AddPeer(node p2p.Peer)                    {}
 
 func TestGrpcMessageHandler_HandleMessageReceive(t *testing.T) {
 
 	leader := p2p.Leader{}
 	leaderByte, _ := json.Marshal(leader)
 
-	nodeList := make([]p2p.Node, 0)
-	newNodeList := append(nodeList, p2p.Node{NodeId: p2p.NodeId{Id: "123"}})
-	nodeListByte, _ := json.Marshal(newNodeList)
+	nodeList := make([]p2p.Peer, 0)
+	newPeerList := append(nodeList, p2p.Peer{PeerId: p2p.PeerId{Id: "123"}})
+	nodeListByte, _ := json.Marshal(newPeerList)
 
 	tests := map[string]struct {
 		input struct {
@@ -55,14 +55,14 @@ func TestGrpcMessageHandler_HandleMessageReceive(t *testing.T) {
 				command: p2p.GrpcRequestCommand{
 					CommandModel: midgard.CommandModel{ID: "123"},
 					Data:         nodeListByte,
-					Protocol:     "NodeListDeliverProtocol",
+					Protocol:     "PeerListDeliverProtocol",
 				},
 			},
 			err: nil,
 		},
 	}
 	leaderApi := MockLeaderApi{}
-	nodeApi := MockNodeApi{}
+	nodeApi := MockPeerApi{}
 	messageHandler := adapter.NewGrpcCommandHandler(leaderApi, nodeApi)
 
 	for testName, test := range tests {
