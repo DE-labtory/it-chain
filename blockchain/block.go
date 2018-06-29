aa1225012c9437c151fd3bafb42664857c1072cc
junk-sound <wom2277@naver.com>
2018-06-30 02:35:03 +0900

    rename Repository -> BlockRepository

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,6 +1,12 @@
 package blockchain
 
 import (
+	"encoding/json"
+	"time"
+
+	"io/ioutil"
+	"os"
+
 	"github.com/it-chain/yggdrasill"
 	"github.com/it-chain/yggdrasill/common"
 	"github.com/it-chain/yggdrasill/impl"
@@ -10,8 +16,48 @@ type Block = common.Block
 
 type DefaultBlock = impl.DefaultBlock
 
-type Repository interface {
+type BlockRepository interface {
 	yggdrasill.BlockStorageManager
 	NewEmptyBlock() (Block, error)
 	GetBlockCreator() string
+}
+
+func CreateGenesisBlock(genesisconfFilePath string) (*impl.DefaultBlock, error) {
+	byteValue, err := ConfigFromJson(genesisconfFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	validator := new(impl.DefaultValidator)
+
+	var GenesisBlock *impl.DefaultBlock
+
+	json.Unmarshal(byteValue, &GenesisBlock)
+	GenesisBlock.SetTimestamp((time.Now()).Round(0))
+	Seal, err := validator.BuildSeal(GenesisBlock)
+	if err != nil {
+		return nil, err
+	}
+
+	GenesisBlock.SetSeal(Seal)
+	GenesisBlock.SetPrevSeal(GenesisBlock.PrevSeal)
+	GenesisBlock.SetHeight(GenesisBlock.Height)
+	GenesisBlock.SetHeight(GenesisBlock.Height)
+	GenesisBlock.SetTxSeal(GenesisBlock.TxSeal)
+	GenesisBlock.SetCreator(GenesisBlock.Creator)
+	return GenesisBlock, nil
+}
+
+func ConfigFromJson(filePath string) ([]uint8, error) {
+	jsonFile, err := os.Open(filePath)
+	defer jsonFile.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+	return byteValue, nil
 }
