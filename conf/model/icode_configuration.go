d239323d07d2cca8854f76084d64ad5d9c06dcaf
ParkHaeSung <hea9549@gmail.com>
2018-06-30 15:30:24 +0900

    add icode save path config, fin git store api

--- conf/model/icode_configuration.go
+++ conf/model/icode_configuration.go
@@ -3,11 +3,13 @@ package model
 type ICodeConfiguration struct {
 	RepositoryPath string
 	SshPath        string
+	ICodeSavePath  string
 }
 
 func NewIcodeConfiguration() ICodeConfiguration {
 	return ICodeConfiguration{
 		RepositoryPath: "empty",
 		SshPath:        "default", // set ssh path or default. default mean HomeDir/.ssh/id_rsa
+		ICodeSavePath:  "./.tmp",
 	}
 }
--- icode/infra/api/icode_git_store.go
+++ icode/infra/api/icode_git_store.go
@@ -7,18 +7,27 @@ import (
 
 	"os/user"
 
+	"errors"
+	"fmt"
+
+	"os"
+
 	"github.com/google/go-github/github"
 	"github.com/it-chain/it-chain-Engine/conf"
 	"github.com/it-chain/it-chain-Engine/icode"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/config"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
 	"gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
 )
 
 type ICodeGitStoreApi struct {
-	authClient *github.Client
-	authUserID string
-	authUserPW string
-	name       string
-	sshAuth    *ssh.PublicKeys
+	authClient  *github.Client
+	AuthGitUser *github.User
+	authUserID  string
+	authUserPW  string
+	name        string
+	sshAuth     *ssh.PublicKeys
 }
 
 func NewICodeGitStoreApi(authUserID string, authUserPW string) (*ICodeGitStoreApi, error) {
@@ -49,18 +58,121 @@ func NewICodeGitStoreApi(authUserID string, authUserPW string) (*ICodeGitStoreAp
 	}
 
 	return &ICodeGitStoreApi{
-		authClient: client,
-		authUserID: authUserID,
-		authUserPW: authUserPW,
-		name:       gitUser.GetLogin(),
-		sshAuth:    sshAuth,
+		authClient:  client,
+		AuthGitUser: gitUser,
+		authUserID:  authUserID,
+		authUserPW:  authUserPW,
+		name:        gitUser.GetLogin(),
+		sshAuth:     sshAuth,
 	}, nil
 }
 
-func (*ICodeGitStoreApi) Clone(repositoryUrl string) (*icode.Meta, error) {
-	panic("implement me")
+func (gApi *ICodeGitStoreApi) Clone(repositoryUrl string) (*icode.Meta, error) {
+	name := getNameFromGitUrl(repositoryUrl)
+
+	if name == "" {
+		return nil, errors.New(fmt.Sprintf("Invalid url name [%s]", repositoryUrl))
+	}
+
+	r, err := git.PlainClone(conf.GetConfiguration().Icode.ICodeSavePath+"/"+name, false, &git.CloneOptions{
+		URL:               repositoryUrl,
+		Auth:              gApi.sshAuth,
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	head, err := r.Head()
+
+	if err != nil {
+		return nil, err
+	}
+
+	lastHeadCommit, err := r.CommitObject(head.Hash())
+	commitHash := lastHeadCommit.Hash.String()
+
+	if err != nil {
+		return nil, err
+	}
+
+	metaData := icode.NewMeta(name, repositoryUrl, conf.GetConfiguration().Icode.ICodeSavePath+"/"+name, commitHash)
+	return metaData, nil
 }
 
-func (*ICodeGitStoreApi) Push(meta icode.Meta) error {
-	panic("implement me")
+//todo async push
+func (gApi *ICodeGitStoreApi) Push(meta icode.Meta) error {
+	iCodePath := meta.Path
+
+	if _, err := os.Stat(iCodePath); os.IsNotExist(err) {
+		return errors.New(fmt.Sprintf("Invalid iCodeMeta Path [%s]", iCodePath))
+	}
+
+	err := changeRemote(iCodePath, gApi.AuthGitUser.GetHTMLURL()+"/"+meta.RepositoryName)
+
+	if err != nil {
+		return err
+	}
+
+	au := &http.BasicAuth{Username: gApi.authUserID, Password: gApi.authUserPW}
+
+	r, err := git.PlainOpen(iCodePath)
+
+	if err != nil {
+		return err
+	}
+
+	err = r.Push(&git.PushOptions{
+		RemoteName: git.DefaultRemoteName,
+		Auth:       au,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func getNameFromGitUrl(gitUrl string) string {
+	parsed := strings.Split(gitUrl, "/")
+
+	if len(parsed) == 0 {
+		return ""
+	}
+
+	name := strings.Split(parsed[len(parsed)-1], ".")
+
+	if len(name) == 0 {
+		return ""
+	}
+
+	return name[0]
+}
+
+func changeRemote(path string, changeUrl string) error {
+	r, err := git.PlainOpen(path)
+
+	if err != nil {
+		return err
+	}
+
+	err = r.DeleteRemote(git.DefaultRemoteName)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = r.CreateRemote(&config.RemoteConfig{
+		Name: git.DefaultRemoteName,
+		URLs: []string{changeUrl},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
