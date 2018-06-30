ac79dc0cb8c954ed7a6007055e279ee6ded867b4
ParkHaeSung <hea9549@gmail.com>
2018-06-30 14:03:47 +0900

    before merge develope branch

--- icode/api/icode_api.go
+++ icode/api/icode_api.go
@@ -1,1 +1,13 @@
 package api
+
+import "github.com/it-chain/it-chain-Engine/icode"
+
+type ICodeApi struct {
+	ContainerService icode.ContainerService
+	StoreApi         ICodeStoreApi
+	MetaRepository   icode.MetaRepository
+}
+
+func (iApi ICodeApi)()  {
+
+}
