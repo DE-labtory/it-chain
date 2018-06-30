7503d64c2092c102074ef7403540c23228bf3cd9
jun <leebduk@gmail.com>
2018-06-30 15:03:11 +0900

    event to domain layer

--- gateway/connection.go
+++ gateway/connection.go
@@ -1,8 +1,32 @@
 package gateway
 
-import "github.com/it-chain/midgard"
+import (
+	"github.com/it-chain/it-chain-Engine/core/eventstore"
+	"github.com/it-chain/midgard"
+)
 
 type Connection struct {
 	midgard.AggregateModel
 	Address string
+}
+
+func AddConnection(connection Connection) error {
+
+	return eventstore.Save(connection.ID, ConnectionCreatedEvent{
+		EventModel: midgard.EventModel{
+			ID:   connection.ID,
+			Type: "connection.created",
+		},
+		Address: connection.Address,
+	})
+}
+
+func CloseConnection(connectionID string) error {
+
+	return eventstore.Save(connectionID, ConnectionClosedEvent{
+		EventModel: midgard.EventModel{
+			ID:   connectionID,
+			Type: "connection.closed",
+		},
+	})
 }
