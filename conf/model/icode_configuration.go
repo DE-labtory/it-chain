a95073ced6922ec4fd52f69bceb4375255ad7d4f
ParkHaeSung <hea9549@gmail.com>
2018-06-30 15:01:21 +0900

    modify and generate config file for auth, icode

--- conf/model/icode_configuration.go
+++ conf/model/icode_configuration.go
@@ -2,10 +2,12 @@ package model
 
 type ICodeConfiguration struct {
 	RepositoryPath string
+	SshPath        string
 }
 
 func NewIcodeConfiguration() ICodeConfiguration {
 	return ICodeConfiguration{
 		RepositoryPath: "empty",
+		SshPath:        "default", // set ssh path or default. default mean HomeDir/.ssh/id_rsa
 	}
 }
