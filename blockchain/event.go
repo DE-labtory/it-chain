05af93f28ee8d8912cd10fe6c42f0afc28629286
junk-sound <wom2277@naver.com>
2018-07-02 20:21:49 +0900

    delete trash: blockProposedEvent, blockValidatedEvent

--- blockchain/event.go
+++ blockchain/event.go
@@ -15,13 +15,3 @@ type NodeDeletedEvent struct {
 	midgard.EventModel
 	Peer
 }
-
-type BlockValidatedEvent struct {
-	midgard.EventModel
-	Block Block
-}
-
-type BlockProposedEvent struct {
-	midgard.EventModel
-	Block Block
-}
