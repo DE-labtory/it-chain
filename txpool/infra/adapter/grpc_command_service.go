6182e9e93978dbed5459f68135c9508d848b481a
zeroFruit <14wnrkim@gmail.com>
2018-07-02 17:15:36 +0900

    SendLeaderTransactions test case

--- txpool/infra/adapter/grpc_command_service.go
+++ txpool/infra/adapter/grpc_command_service.go
@@ -9,6 +9,8 @@ import (
 	"github.com/it-chain/it-chain-Engine/common"
 )
 
+var ErrTxEmpty = errors.New("Empty transaction list proposed")
+
 type Publisher func(exchange string, topic string, data interface{}) (err error) //해당 publish함수는 midgard 에서 의존성 주입을 받기 위해 interface로 작성한다.
 //모든 의존성 주입은 컴포넌트.go 에서 이루어짐
 
@@ -25,7 +27,7 @@ func NewGrpcCommandService(publisher Publisher) *GrpcCommandService {
 func (gcs GrpcCommandService) SendLeaderTransactions(transactions []*txpool.Transaction, leader txpool.Leader) error {
 
 	if len(transactions) == 0 {
-		return errors.New("Empty transaction list proposed")
+		return ErrTxEmpty
 	}
 
 	deliverCommand, err := createGrpcDeliverCommand("SendLeaderTransactionsProtocol", transactions)
