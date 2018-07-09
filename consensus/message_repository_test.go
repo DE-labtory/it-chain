cd65ede63863d9d9670ef7c93c4f9f1cc4af5fd6
KimJunHui <junhui820@gmail.com>
2018-07-09 21:42:16 +0900

    Edited message repository for commit msg and test cases

--- consensus/message_repository_test.go
+++ consensus/message_repository_test.go
@@ -49,3 +49,45 @@ func TestPrepareMsgRepositoryImpl_Remove(t *testing.T) {
 	// Then
 	assert.Equal(t, 0, len(prepareMsgRepositoryImpl.PreparePool))
 }
+
+func TestCommitMsgRepositoryImpl_Save(t *testing.T) {
+	// Given
+	prepareMsgRepository := NewPrepareMsgRepository()
+
+	consensusId := NewConsensusId("1")
+
+	prepareMsg := PrepareMsg{
+		ConsensusId: consensusId,
+		SenderId:    "1",
+	}
+
+	// When
+	prepareMsgRepository.Save(prepareMsg)
+
+	// Then
+	assert.Equal(t, 1, len(prepareMsgRepository.FindPrepareMsgsByConsensusID(consensusId)))
+}
+
+func TestCommitMsgRepositoryImpl_Remove(t *testing.T) {
+	// Given
+	commitMsgRepository := NewCommitMsgRepository()
+	commitMsgRepositoryImpl := CommitMsgRepositoryImpl{
+		CommitPool: make(map[ConsensusId][]CommitMsg, 0),
+		lock:       &sync.RWMutex{},
+	}
+	commitMsgRepository = &commitMsgRepositoryImpl
+
+	consensusId := NewConsensusId("1")
+
+	commitMsg := CommitMsg{
+		ConsensusId: consensusId,
+		SenderId:    "1",
+	}
+
+	// When
+	commitMsgRepository.Save(commitMsg)
+	commitMsgRepository.Remove(consensusId)
+
+	// Then
+	assert.Equal(t, 0, len(commitMsgRepositoryImpl.CommitPool))
+}
