08e0e6d3ca97c37e5ad372bbc6af7dda350b028c
KimJunHui <junhui820@gmail.com>
2018-07-12 18:18:07 +0900

    Define commands

--- consensus/command.go
+++ consensus/command.go
@@ -1,1 +1,22 @@
 package consensus
+
+import "github.com/it-chain/midgard"
+
+type CreateBlockCommand struct {
+	midgard.CommandModel
+}
+
+type SendPrePrepareMsgCommand struct {
+	midgard.CommandModel
+	PrePrepareMsg PrePrepareMsg
+}
+
+type SendPrepareMsgCommand struct {
+	midgard.CommandModel
+	PrepareMsg PrepareMsg
+}
+
+type SendCommitMsgCommand struct {
+	midgard.CommandModel
+	CommitMsg CommitMsg
+}
