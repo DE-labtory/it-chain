b8f0c3506303afe4cea200613be996f0b22a5010
junk-sound <wom2277@naver.com>
2018-07-18 12:56:24 +0900

    changed GenesisBlock timestamp

--- blockchain/block_factory.go
+++ blockchain/block_factory.go
@@ -32,7 +32,6 @@ func CreateGenesisBlock(genesisconfFilePath string) (Block, error) {
 	//declare
 	GenesisBlock := &DefaultBlock{}
 	validator := DefaultValidator{}
-	TimeStamp := (time.Now()).Round(0)
 
 	//set
 	err := setBlockWithConfig(genesisconfFilePath, GenesisBlock)
@@ -42,14 +41,14 @@ func CreateGenesisBlock(genesisconfFilePath string) (Block, error) {
 	}
 
 	//build
-	Seal, err := validator.BuildSeal(TimeStamp, GenesisBlock.PrevSeal, GenesisBlock.TxSeal, GenesisBlock.Creator)
+	Seal, err := validator.BuildSeal(GenesisBlock.Timestamp, GenesisBlock.PrevSeal, GenesisBlock.TxSeal, GenesisBlock.Creator)
 
 	if err != nil {
 		return nil, ErrBuildingSeal
 	}
 
 	//create
-	createEvent, err := createBlockCreatedEvent(Seal, GenesisBlock.PrevSeal, GenesisBlock.Height, convertTxType(GenesisBlock.TxList), GenesisBlock.TxSeal, TimeStamp, GenesisBlock.Creator)
+	createEvent, err := createBlockCreatedEvent(Seal, GenesisBlock.PrevSeal, GenesisBlock.Height, convertTxType(GenesisBlock.TxList), GenesisBlock.TxSeal, GenesisBlock.Timestamp, GenesisBlock.Creator)
 	if err != nil {
 		return nil, ErrCreatingEvent
 	}
