c5d4a237388f78d91142aa1cab4fd0493e384bfe
zeroFruit <14wnrkim@gmail.com>
2018-07-18 11:25:06 +0900

    CommitedBlockRepository, Impl declaration

---
+++ api_gateway/block_query_api.go
@@ -0,0 +1,22 @@
+package api_gateway
+
+import (
+	"github.com/it-chain/it-chain-Engine/blockchain"
+	"sync"
+	"github.com/it-chain/yggdrasill"
+)
+
+type BlockQueryApi struct {
+	CommitedBlockRepository
+}
+
+type CommitedBlockRepository interface {
+	Save(block blockchain.Block) error
+	GetLastBlock(block blockchain.Block) (blockchain.Block, error)
+	GetBlockByHeight(height blockchain.BlockHeight) (blockchain.Block, error)
+}
+
+type CommitedBlockRepositoryImpl struct {
+	mux *sync.RWMutex
+	yggdrasill.BlockStorageManager
+}
