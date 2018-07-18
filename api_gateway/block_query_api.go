c64cc98c8657d59f155029b88723d21d3ae00f82
zeroFruit <14wnrkim@gmail.com>
2018-07-18 11:13:52 +0900

    declare interface

---
+++ api_gateway/block_query_api.go
@@ -0,0 +1,13 @@
+package api_gateway
+
+import "github.com/it-chain/it-chain-Engine/blockchain"
+
+type BlockQueryApi struct {
+	BlockPoolRepository
+}
+
+type BlockPoolRepository interface {
+	AddCreatedBlock(block blockchain.Block)
+	GetStagedBlockByHeight(height blockchain.BlockHeight) (blockchain.Block, error)
+	GetStagedBlockById(id string) (blockchain.Block, error)
+}
