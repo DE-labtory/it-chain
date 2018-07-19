87e4a3dfd7d87c2dafc113d75b1bee587d8f5182
zeroFruit <14wnrkim@gmail.com>
2018-07-19 16:43:45 +0900

    remove action, move it to block service

--- blockchain/service.go
+++ blockchain/service.go
@@ -10,3 +10,51 @@ type BlockQueryInnerService interface {
 	GetLastCommitedBlock() (Block, error)
 	GetCommitedBlockByHeight(height BlockHeight) (Block, error)
 }
+
+type BlockCreateService interface {
+	CreateBlock(block Block) error
+}
+
+type BlockCreateServiceImpl struct{}
+
+func NewBlockCreateService() *BlockCreateServiceImpl {
+	return &BlockCreateServiceImpl{}
+}
+
+func (bcs *BlockCreateServiceImpl) CreateBlock(block Block) error {
+	// create BlockCreatedEvent
+	// save it to event store
+	return nil
+}
+
+type BlockStageService interface {
+	StageBlock(block Block) error
+}
+
+type BlockStageServiceImpl struct{}
+
+func NewBlockStageService() *BlockStageServiceImpl {
+	return &BlockStageServiceImpl{}
+}
+
+func (bss *BlockStageServiceImpl) StageBlock(block Block) error {
+	// create BlockStageEvent
+	// save it to event store
+	return nil
+}
+
+type BlockCommitService interface {
+	CommitBlock(block Block) error
+}
+
+type BlockCommitServiceImpl struct{}
+
+func NewBlockCommitService() *BlockCommitServiceImpl {
+	return &BlockCommitServiceImpl{}
+}
+
+func (bcs *BlockCommitServiceImpl) CommitBlock(block Block) error {
+	// create BlockCommitEvent
+	// save it to event store
+	return nil
+}
