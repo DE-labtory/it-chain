6510466b5ea10d42ef13bea332694de9be5c9abd
ParkHaeSung <hea9549@gmail.com>
2018-07-19 18:44:53 +0900

    make icode query api

--- api_gateway/icode_query_api.go
+++ api_gateway/icode_query_api.go
@@ -18,19 +18,153 @@ package api_gateway
 
 import (
 	"github.com/it-chain/engine/icode"
+	"github.com/it-chain/leveldb-wrapper"
+	"github.com/it-chain/engine/common"
+	"errors"
+	"log"
+	"fmt"
 )
 
-// this is an api only for querying current state which is repository of transaction
 type ICodeQueryApi struct {
-	transactionRepository ICodeMetaRepository
+	metaRepository ICodeMetaRepository
 }
 
-// this repository is a current state of all uncommitted transactions
+func NewICodeQueryApi(repository ICodeMetaRepository) ICodeQueryApi {
+	return ICodeQueryApi{
+		metaRepository: repository,
+	}
+}
+
 type ICodeMetaRepository interface {
-	FindAllMeta() []icode.Meta
-	Save(transaction icode.Meta)
+	FindAllMeta() ([]icode.Meta, error)
+	FindMetaByUrl(url string) (icode.Meta, error)
+	FindMetaById(id icode.ID) (icode.Meta, error)
+	Save(meta icode.Meta) error
+	Remove(id icode.ID) error
+}
+
+type LevelDbMetaRepository struct {
+	leveldb *leveldbwrapper.DB
+}
+
+func NewLevelDbMetaRepository(path string) LevelDbMetaRepository {
+	db := leveldbwrapper.CreateNewDB(path)
+	db.Open()
+	return LevelDbMetaRepository{
+		leveldb: db,
+	}
+}
+
+func (l *LevelDbMetaRepository) FindAllMeta() ([]icode.Meta, error) {
+	iter := l.leveldb.GetIteratorWithPrefix([]byte(""))
+	metaList := []icode.Meta{}
+	for iter.Next() {
+		val := iter.Value()
+		meta := &icode.Meta{}
+		err := common.Deserialize(val, meta)
+		if err != nil {
+			return nil, err
+		}
+		metaList = append(metaList, *meta)
+	}
+	return metaList, nil
+}
+
+func (l *LevelDbMetaRepository) FindMetaByUrl(url string) (icode.Meta, error) {
+	allMetaList, err := l.FindAllMeta()
+	if err != nil {
+		return icode.Meta{}, err
+	}
+
+	for _, meta := range allMetaList {
+		if meta.GitUrl == url {
+			return meta, nil
+		}
+	}
+
+	return icode.Meta{}, nil
+}
+
+func (l *LevelDbMetaRepository) FindMetaById(id icode.ID) (icode.Meta, error) {
+
+	metaByte, err := l.leveldb.Get([]byte(id))
+	if err != nil {
+		return icode.Meta{}, err
+	}
+
+	if len(metaByte) == 0 {
+		return icode.Meta{}, nil
+	}
+
+	meta := &icode.Meta{}
+
+	err = common.Deserialize(metaByte,meta)
+
+	if err != nil {
+		return icode.Meta{}, err
+	}
+
+	return *meta, nil
+}
+
+func (l *LevelDbMetaRepository) Save(meta icode.Meta) error {
+	if meta.GetID() == "" {
+		return errors.New("meta is empty")
+	}
+	b, err := common.Serialize(meta)
+	if err != nil {
+		return err
+	}
+
+	err = l.leveldb.Put([]byte(meta.GetID()), b, true)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (l *LevelDbMetaRepository) Remove(id icode.ID) error {
+	return l.leveldb.Delete([]byte(id), true)
 }
 
 type ICodeEventHandler struct {
-	repo ICodeMetaRepository
+	metaRepository ICodeMetaRepository
+}
+
+func NewIcodeEvnetHandler(repository ICodeMetaRepository) ICodeEventHandler {
+	return ICodeEventHandler{
+		metaRepository: repository,
+	}
+}
+
+func (i ICodeEventHandler) HandleMetaCreatedEvent(event icode.MetaCreatedEvent) {
+	meta := event.GetMeta()
+	err := i.metaRepository.Save(meta)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func (i ICodeEventHandler) HandleMetaDeletedEvent(event icode.MetaDeletedEvent) {
+	err := i.metaRepository.Remove(event.GetID())
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func (i ICodeEventHandler) HandleMetaStatusChangeEvent(event icode.MetaStatusChangeEvent) {
+	meta, err := i.metaRepository.FindMetaById(event.GetID())
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	if meta.GetID() ==""{
+		log.Fatal(fmt.Sprintf("no icode id : [%s] in handleMetaStatusChangeEvent(api_gateway/icode_query_api",event.GetID()))
+	}
+	meta.Status = event.Status
+	err = i.metaRepository.Save(meta)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
