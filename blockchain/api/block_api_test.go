3ceda6b3764b6f813c7b26f47a35afac60839b1a
zeroFruit <14wnrkim@gmail.com>
2018-07-19 10:26:38 +0900

    fix test case

--- blockchain/api/block_api_test.go
+++ blockchain/api/block_api_test.go
@@ -1,14 +1,10 @@
 package api_test
 
 import (
-	"errors"
-	"fmt"
 	"testing"
 
 	"github.com/it-chain/it-chain-Engine/blockchain"
 	"github.com/it-chain/it-chain-Engine/blockchain/api"
-	"github.com/it-chain/it-chain-Engine/blockchain/test/mock"
-	"github.com/it-chain/midgard"
 	"github.com/magiconair/properties/assert"
 )
 
@@ -27,16 +23,9 @@ func TestBlockApi_AddBlockToPool(t *testing.T) {
 		},
 	}
 
-	blockRepository := mock.BlockQueryApi{}
 	publisherId := "zf"
-	eventRepository := mock.EventRepository{}
-	eventRepository.LoadFunc = func(aggregate midgard.Aggregate, aggregateID string) error {
-		// predefine block pool
-		aggregate = blockchain.NewBlockPool()
-		return nil
-	}
 
-	blockApi, _ := api.NewBlockApi(blockRepository, eventRepository, publisherId)
+	blockApi, _ := api.NewBlockApi(publisherId)
 
 	for testName, test := range tests {
 		t.Logf("running test case %s", testName)
@@ -58,43 +47,11 @@ func TestBlockApi_CheckAndSaveBlockFromPool(t *testing.T) {
 			}{height: blockchain.BlockHeight(12)},
 			err: nil,
 		},
-		"block nil test": {
-			input: struct {
-				height blockchain.BlockHeight
-			}{height: blockchain.BlockHeight(144)},
-			err: api.ErrNilBlock,
-		},
-	}
-	// When
-	blockQueryApi := mock.BlockQueryApi{}
-	blockQueryApi.GetLastBlockFunc = func() (blockchain.Block, error) {
-		return &blockchain.DefaultBlock{
-			Height: blockchain.BlockHeight(12),
-		}, nil
 	}
 	publisherId := "zf"
-	eventRepository := mock.EventRepository{}
-	eventRepository.LoadFunc = func(aggregate midgard.Aggregate, aggregateID string) error {
-		switch v := aggregate.(type) {
-		case blockchain.BlockPool:
-			aggregate.(blockchain.BlockPool).Add(&blockchain.DefaultBlock{
-				Height: blockchain.BlockHeight(12),
-				TxList: []*blockchain.DefaultTransaction{},
-			})
-			break
-
-		case blockchain.SyncState:
-			aggregate.(blockchain.SyncState).SetProgress(blockchain.DONE)
-			break
-		default:
-			return errors.New(fmt.Sprintf("unhandled type [%s]", v))
-		}
-
-		return nil
-	}
 
 	// When
-	blockApi, _ := api.NewBlockApi(blockQueryApi, eventRepository, publisherId)
+	blockApi, _ := api.NewBlockApi(publisherId)
 
 	for testName, test := range tests {
 		t.Logf("running test case %s", testName)
@@ -107,22 +64,12 @@ func TestBlockApi_CheckAndSaveBlockFromPool(t *testing.T) {
 	}
 }
 
-func TestBlockApi_SyncedCheck(t *testing.T) {
-	// TODO:
-}
-
 func TestBlockApi_SyncIsProgressing(t *testing.T) {
 	// when
-	blockQueryApi := mock.BlockQueryApi{}
-	eventRepository := mock.EventRepository{}
-	eventRepository.LoadFunc = func(aggregate midgard.Aggregate, aggregateID string) error {
-		assert.Equal(t, blockchain.BC_SYNC_STATE_AID, aggregateID)
-		return nil
-	}
 	publisherId := "zf"
 
 	// when
-	blockApi, _ := api.NewBlockApi(blockQueryApi, eventRepository, publisherId)
+	blockApi, _ := api.NewBlockApi(publisherId)
 
 	// then
 	state := blockApi.SyncIsProgressing()
