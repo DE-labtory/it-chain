90855e786f605b8a0d66803ac7b844ce197f934d
jun <leebduk@gmail.com>
2018-07-19 14:36:59 +0900

    fix timeout batch test issue

--- txpool/infra/batch/timeout_batcher_test.go
+++ txpool/infra/batch/timeout_batcher_test.go
@@ -1,7 +1,6 @@
 package batch_test
 
 import (
-	"errors"
 	"testing"
 	"time"
 
@@ -41,43 +40,21 @@ func TestTimeoutBatcher_Run(t *testing.T) {
 
 					return nil
 				},
-				duration: time.Second * 1,
-			},
-			err: nil,
-		},
-		"timer return error": {
-			input: struct {
-				taskFunc batch.TaskFunc
-				duration time.Duration
-			}{
-				taskFunc: func() error {
-
-					if counter == 0 {
-						wg.Done()
-						fmt.Println("done")
-					}
-
-					counter++
-
-					return errors.New("asd")
-				},
-				duration: time.Second * 1,
+				duration: time.Second * 3,
 			},
 			err: nil,
 		},
 	}
 
 	batcher := batch.GetTimeOutBatcherInstance()
+	wg.Add(1)
 
 	for testName, test := range tests {
 		t.Logf("Running test case %s", testName)
 
 		//when
-		wg.Add(1)
 		counter = 0
 		quit := batcher.Run(test.input.taskFunc, test.input.duration)
-
-		fmt.Println("wait")
 		wg.Wait()
 		quit <- struct{}{}
 	}
