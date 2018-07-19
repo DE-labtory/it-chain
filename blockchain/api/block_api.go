9781587a309890bdafb5bb43503a47eb3137f777
zeroFruit <14wnrkim@gmail.com>
2018-07-19 10:23:32 +0900

    clean block api

--- blockchain/api/block_api.go
+++ blockchain/api/block_api.go
@@ -2,20 +2,15 @@ package api
 
 import (
 	"github.com/it-chain/it-chain-Engine/blockchain"
-	"github.com/it-chain/midgard"
 )
 
 type BlockApi struct {
-	blockQueryApi   blockchain.BlockQueryApi
-	eventRepository midgard.EventRepository
-	publisherId     string
+	publisherId string
 }
 
-func NewBlockApi(blockQueryApi blockchain.BlockQueryApi, eventRepository midgard.EventRepository, publisherId string) (BlockApi, error) {
+func NewBlockApi(publisherId string) (BlockApi, error) {
 	return BlockApi{
-		blockQueryApi:   blockQueryApi,
-		eventRepository: eventRepository,
-		publisherId:     publisherId,
+		publisherId: publisherId,
 	}, nil
 }
 
@@ -26,54 +21,19 @@ func (bApi *BlockApi) SyncedCheck(block blockchain.Block) error {
 
 // 받은 block을 block pool에 추가한다.
 func (bApi *BlockApi) AddBlockToPool(block blockchain.Block) error {
-	if block == nil {
-		return ErrNilBlock
-	}
-
-	pool := bApi.loadBlockPool()
-	err := pool.Add(block)
-	if err != nil {
-		return err
-	}
 	return nil
 }
 
 func (bApi *BlockApi) CheckAndSaveBlockFromPool(height blockchain.BlockHeight) error {
-	if bApi.SyncIsProgressing() {
-		return ErrSyncProcessing
-	}
-
-	pool := bApi.loadBlockPool()
-
-	blockFromPool := pool.Get(height)
-	if blockFromPool == nil {
-		return ErrNilBlock
-	}
-
-	lastBlock, err := bApi.blockQueryApi.GetLastBlock()
-	if err != nil {
-		return ErrGetLastBlock
-	}
-
-	isSafeToSave := compareHeight(blockFromPool.GetHeight(), lastBlock.GetHeight())
-
-	action := blockchain.CreateSaveOrSyncAction(isSafeToSave)
-
-	action.DoAction(blockFromPool)
-
 	return nil
 }
 
 func (bApi *BlockApi) SyncIsProgressing() blockchain.ProgressState {
-	syncState := blockchain.NewBlockSyncState()
-	bApi.eventRepository.Load(syncState, blockchain.BC_SYNC_STATE_AID)
-	return syncState.IsProgressing()
+	return blockchain.DONE
 }
 
 func (bApi *BlockApi) loadBlockPool() blockchain.BlockPool {
-	pool := blockchain.NewBlockPool()
-	bApi.eventRepository.Load(pool, blockchain.BLOCK_POOL_AID)
-	return pool
+	return nil
 }
 
 func compareHeight(height1 uint64, height2 uint64) int64 {
