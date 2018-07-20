515d0c02380356f450b719785a211bdcc0cff5af
junk-sound <wom2277@naver.com>
2018-07-20 14:33:59 +0900

    CommitBlock test

---
+++ blockchain/service_test.go
@@ -0,0 +1,61 @@
+package blockchain_test
+
+import (
+	"testing"
+
+	"github.com/it-chain/engine/blockchain"
+	"github.com/it-chain/engine/blockchain/test/mock"
+	"github.com/it-chain/engine/core/eventstore"
+	"github.com/it-chain/midgard"
+	"github.com/magiconair/properties/assert"
+)
+
+func TestCommitBlock(t *testing.T) {
+
+	//given
+	tests := map[string]struct {
+		input *blockchain.DefaultBlock
+		err   error
+	}{
+		"success": {
+			input: &blockchain.DefaultBlock{
+				Seal:  []byte("Seal"),
+				State: blockchain.Created,
+			},
+			err: nil,
+		},
+	}
+
+	for testName, test := range tests {
+		t.Logf("Running test case %s", testName)
+
+		repo := mock.EventRepository{}
+
+		repo.SaveFunc = func(aggregateID string, events ...midgard.Event) error {
+
+			assert.Equal(t, string(test.input.GetSeal()), events[0].GetID())
+			assert.Equal(t, 1, len(events))
+
+			//when
+			test.input.On(events[0])
+
+			//then
+			assert.Equal(t, blockchain.Committed, test.input.State)
+
+			return nil
+		}
+
+		repo.CloseFunc = func() {}
+
+		eventstore.InitForMock(repo)
+		defer eventstore.Close()
+
+		//when
+		err := blockchain.CommitBlock(test.input)
+
+		//then
+		assert.Equal(t, test.err, err)
+
+	}
+
+}
