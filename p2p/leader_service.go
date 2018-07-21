d5a8d0566b7fc28c6b0a79bdf26283265f69bd05
namhoon <frontalnh@gmail.com>
2018-07-21 18:32:06 +0900

    update leader service

--- p2p/leader_service.go
+++ p2p/leader_service.go
@@ -27,6 +27,11 @@ type ILeaderService interface {
 
 type LeaderService struct{}
 
+func NewLeaderService() *LeaderService {
+
+	return &LeaderService{}
+}
+
 func (ls *LeaderService) Set(leader Leader) error {
 
 	event := LeaderUpdatedEvent{
