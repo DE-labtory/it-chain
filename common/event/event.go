5d2b82f0b83bba342b0aff0f125d9cfc636dd75e
KimJunHui <junhui820@gmail.com>
2018-07-21 16:55:50 +0900

    Made commands and events in common pkg.

---
+++ common/event/event.go
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2018 It-chain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package event
+
+import "github.com/it-chain/midgard"
+
+/*
+ * consensus
+ */
+
+// Publish part
+
+type PrepareMsgAddedEvent struct {
+	midgard.EventModel
+	SenderId  string
+	BlockHash []byte
+}
+
+type CommitMsgAddedEvent struct {
+	midgard.EventModel
+	SenderId string
+}
+
+type ConsensusCreatedEvent struct {
+	midgard.EventModel
+	ConsensusId     string
+	Representatives []*string
+	Seal            []byte
+	Body            []byte
+	CurrentState    string
+}
+
+// Preprepare msg를 보냈을 때
+type ConsensusPrePreparedEvent struct {
+	midgard.EventModel
+}
+
+// Prepare msg를 보냈을 때
+type ConsensusPreparedEvent struct {
+	midgard.EventModel
+}
+
+// Commit msg를 보냈을 때
+type ConsensusCommittedEvent struct {
+	midgard.EventModel
+}
+
+// block 저장이 끝나 state가 idle이 될 때
+type ConsensusFinishedEvent struct {
+	midgard.EventModel
+}
+
+// Consume part
+
+type LeaderChangedEvent struct {
+	midgard.EventModel
+	LeaderId string
+}
+
+type MemberJoinedEvent struct {
+	midgard.EventModel
+	MemberId string
+}
+
+type MemberRemovedEvent struct {
+	midgard.EventModel
+	MemberId string
+}
+
+// block이 저장되었을 때
+type BlockSavedEvent struct {
+	midgard.EventModel
+}
