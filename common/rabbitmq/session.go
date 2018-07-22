636fcb96ca669b2ca2cddca5cb534ad370cf2952
jun <leebduk@gmail.com>
2018-07-22 15:18:11 +0900

    add rpc client

---
+++ common/rabbitmq/session.go
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018 It-chain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package rabbitmq
+
+import "github.com/streadway/amqp"
+
+type Session struct {
+	Conn *amqp.Connection
+	Ch   *amqp.Channel
+}
+
+func CreateSession(rabbitmqUrl string) Session {
+
+	if rabbitmqUrl == "" {
+		rabbitmqUrl = "amqp://guest:guest@localhost:5672/"
+	}
+
+	conn, err := amqp.Dial(rabbitmqUrl)
+
+	if err != nil {
+		panic("Failed to connect to RabbitMQ" + err.Error())
+	}
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return Session{
+		Ch:   ch,
+		Conn: conn,
+	}
+}
+
+func (s Session) Close() {
+
+	if s.Conn != nil {
+		s.Conn.Close()
+	}
+	if s.Ch != nil {
+		s.Ch.Close()
+	}
+}
