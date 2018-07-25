24abed155e0854f2ef36de954330a7dff2bf718d
park hae sung <hea9549@gmail.com>
2018-07-25 21:47:14 +0900

    create new logger and remove old

---
+++ common/logger/log_wrapper.go
@@ -0,0 +1,157 @@
+package logger
+
+import (
+	"os"
+
+	"fmt"
+
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Fields = logrus.Fields
+
+var (
+	stdLogger  = logrus.New()
+	fileLogger = logrus.New()
+)
+
+func init() {
+	initStdLogger(stdLogger)
+	fileLogger = nil
+}
+
+// default std logger is enabled
+func EnableStd(enable bool) {
+	if enable && stdLogger == nil {
+		stdLogger = logrus.New()
+		initStdLogger(stdLogger)
+	} else if enable == false {
+		stdLogger = nil
+	}
+}
+
+//default file logger is disabled
+func EnableFileLogger(enable bool, savePath string) error {
+	if enable {
+		fileLogger = logrus.New()
+		return initFileLogger(fileLogger, savePath)
+	} else {
+		fileLogger = nil
+		return nil
+	}
+}
+
+func Debug(fields *Fields, message string) {
+	if stdLogger != nil {
+		stdLogger.WithFields(*fields).Debug(message)
+	}
+	if fileLogger != nil {
+		fileLogger.WithFields(*fields).Debug(message)
+	}
+}
+func Info(fields *Fields, message string) {
+	if stdLogger != nil {
+		stdLogger.WithFields(*fields).Info(message)
+	}
+	if fileLogger != nil {
+		fileLogger.WithFields(*fields).Info(message)
+	}
+}
+func Warn(fields *Fields, message string) {
+	if stdLogger != nil {
+		fields = addLineInfo(fields)
+		stdLogger.WithFields(*fields).Warn(message)
+	}
+	if fileLogger != nil {
+		fields = addLineInfo(fields)
+		fileLogger.WithFields(*fields).Warn(message)
+	}
+}
+func Fatal(fields *Fields, message string) {
+	if stdLogger != nil {
+		fields = addLineInfo(fields)
+		stdLogger.WithFields(*fields).Fatal(message)
+	}
+	if fileLogger != nil {
+		fields = addLineInfo(fields)
+		fileLogger.WithFields(*fields).Fatal(message)
+	}
+}
+func Error(fields *Fields, message string) {
+	if stdLogger != nil {
+		fields = addLineInfo(fields)
+		stdLogger.WithFields(*fields).Error(message)
+	}
+	if fileLogger != nil {
+		fields = addLineInfo(fields)
+		fileLogger.WithFields(*fields).Error(message)
+	}
+}
+
+func Panic(fields *Fields, message string) {
+	if stdLogger != nil {
+		fields = addLineInfo(fields)
+		stdLogger.WithFields(*fields).Panic(message)
+	}
+	if fileLogger != nil {
+		fields = addLineInfo(fields)
+		fileLogger.WithFields(*fields).Panic(message)
+	}
+}
+
+// formatting support
+func Debugf(fields *Fields, format string, args ...interface{}) {
+	Debug(fields, fmt.Sprintf(format, args...))
+}
+
+func Infof(fields *Fields, format string, args ...interface{}) {
+	Info(fields, fmt.Sprintf(format, args...))
+}
+func Warnf(fields *Fields, format string, args ...interface{}) {
+	Warn(fields, fmt.Sprintf(format, args...))
+}
+func Fatalf(fields *Fields, format string, args ...interface{}) {
+	Fatal(fields, fmt.Sprintf(format, args...))
+}
+func Errorf(fields *Fields, format string, args ...interface{}) {
+	Error(fields, fmt.Sprintf(format, args...))
+}
+func Panicf(fields *Fields, format string, args ...interface{}) {
+	Panic(fields, fmt.Sprintf(format, args...))
+}
+
+func initStdLogger(logger *logrus.Logger) {
+	logger.SetOutput(os.Stdout)
+	logger.Formatter = &logrus.TextFormatter{
+		ForceColors:      true,
+		DisableColors:    false,
+		DisableTimestamp: false,
+		FullTimestamp:    true,
+	}
+}
+
+func initFileLogger(logger *logrus.Logger, savePath string) error {
+
+	file, err := os.OpenFile(savePath, os.O_CREATE|os.O_WRONLY, 0666)
+	if err == nil {
+		logger.Out = file
+	} else {
+		return err
+	}
+	return nil
+}
+
+func addLineInfo(fields *Fields) *Fields {
+	pc, _, _, _ := runtime.Caller(2)
+	dataField := Fields{"err_component": runtime.FuncForPC(pc).Name()}
+	data := make(Fields, len(dataField)+len(*fields))
+	for k, v := range dataField {
+		data[k] = v
+	}
+	for k, v := range *fields {
+		data[k] = v
+	}
+	return &data
+}
