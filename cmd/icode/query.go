5cba47bec20f68e2cae4fbe01462fcb467ca53cd
ParkHaeSung <hea9549@gmail.com>
2018-07-27 14:05:36 +0900

    add icode query command and add it to it-chain.go

---
+++ cmd/icode/query.go
@@ -0,0 +1,71 @@
+package icode
+
+import (
+	"errors"
+
+	"github.com/it-chain/engine/common/command"
+	"github.com/it-chain/engine/common/logger"
+	"github.com/it-chain/engine/common/rabbitmq/rpc"
+	"github.com/it-chain/engine/conf"
+	"github.com/it-chain/engine/icode"
+	"github.com/it-chain/midgard"
+	"github.com/urfave/cli"
+)
+
+func QueryCmd() cli.Command {
+	return cli.Command{
+		Name:  "query",
+		Usage: "it-chain icode query [icode id] [function name] [...args]",
+		Action: func(c *cli.Context) error {
+			if c.NArg() < 3 {
+				return errors.New("not enough args")
+			}
+			icodeId := c.Args().Get(0)
+			functionName := c.Args().Get(1)
+			args := make([]string, 0)
+			for i := 2; i < c.NArg(); i++ {
+				args = append(args, c.Args().Get(i))
+			}
+			query(icodeId, functionName, args)
+
+			return nil
+		},
+	}
+}
+
+func query(id string, functionName string, args []string) {
+
+	config := conf.GetConfiguration()
+	client := rpc.NewClient(config.Engine.Amqp)
+
+	defer client.Close()
+
+	queryCommand := command.Query{
+		CommandModel: midgard.CommandModel{
+			ID: id,
+		},
+		Function: functionName,
+		Args:     args,
+	}
+	logger.Info(nil, "query icode ...")
+
+	err := client.Call("icode.query", queryCommand, func(result icode.Result, err rpc.Error) {
+		if !err.IsNil() {
+			logger.Errorf(nil, "fail to query icode err: [%s]", err.Message)
+			return
+		}
+
+		logger.Infof(nil, "%15s : [%s]", "icodeId", id)
+		logger.Infof(nil, "%15s : [%s]", "function Name", functionName)
+		for i, arg := range args {
+			logger.Infof(nil, "%15s : [%s]", "arg"+string(i), arg)
+		}
+		logger.Infof(nil, "%15s : %t", "success", result.Success)
+		for key, val := range result.Data {
+			logger.Infof(nil, "%s : %s", "[result]"+key, val)
+		}
+	})
+	if err != nil {
+		logger.Fatal(&logger.Fields{"err_msg": err.Error()}, "fatal err in query cmd")
+	}
+}
