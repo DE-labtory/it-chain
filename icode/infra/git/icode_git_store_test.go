f4091f36d90213607f65d216e0c28fb2269341c5
ParkHaeSung <hea9549@gmail.com>
2018-07-27 12:22:42 +0900

    hotfix key test error

--- icode/infra/git/icode_git_store_test.go
+++ icode/infra/git/icode_git_store_test.go
@@ -84,31 +84,31 @@ func generatePriKey(path string) (error, func() error) {
 	}
 
 	key := "-----BEGIN RSA PRIVATE KEY-----\n" +
-		"MIIEpAIBAAKCAQEAsTb2shAhgPcMKDqrmqjU4lzLsIWuzBuaEUQ1gVqNlvLr2Pks\n" +
-		"rKvsRLOyYWhLgq405p8yHzwtaKGvq79rCEC3eLd906vA4WKpScqNJbEguCWapIvC\n" +
-		"PGXCKYkmNO3AwNvbcEKpYjhI2Ypst7nr04fKqHF0yQzyf+uh27099wEM9OOHaqTh\n" +
-		"hRVfN4gWT5SWgoi0V884OpMuG7pYt8jUFZKnU5DpKwE/hIMcrYzjvfv+epnhiIC3\n" +
-		"BGs/zzeXsPldBhXceqfdoDsSjDZqx0dYcWiQSjLwwT1MF2r51MDzfMmoABxuMgjY\n" +
-		"jExK2a0IUYy+ngmR1G2XtCfRaVe0wr6XezDmMwIDAQABAoIBADaEIxYaEkR7O3kw\n" +
-		"u1PDtmHAjETMiz5tC1NeeVtGwSH7rwQ7ezvPU8q6wRhoHjqgXtPHi4LCX3G9s64R\n" +
-		"H9sVFZwETqgMQTTUxiFWN1+uAtPDdbRC7kjoQPfIIkHMFiz+NZ5uU29Mw1Rw2gsX\n" +
-		"He4f6v8wj+29lug1U8CmkeZno1W+GeV5I68bzaxRY8d8l9Xgkz+EGyvlCy26hXnz\n" +
-		"ko2ghayWo+hbC10iJ2viBcLp077EEhbhXJnUx7eO4KGgAk+NUbz63Il/eg23yW42\n" +
-		"Lwlj+DImdpT5AgAL7HLRLhGWVisbDtvuS7zqJ+tTcx4fnYyDL9NddaRWFV/ScWRU\n" +
-		"7e9mYCECgYEA3c/WFvEkDj3grs/oddN7aUn60oBzuLApqDPDOTNsa6WvW5dSvvVO\n" +
-		"xacrRxjviwxktMe29C7A9TZzw3UGm050g0BYBV+aom7/qBNK24MxCipIof5u/0jk\n" +
-		"2nZdHYlK1fyCg3FJSQRxe35S1rQRHNq4h8YZnas6IwaXzsPWJyEodBECgYEAzIdu\n" +
-		"NcG5gyfCPwUOz407hjL6mKUn9xsDpxND1JegBsQi47f8N1d64bwreRq8plDQjzZZ\n" +
-		"5nMP5GATEtCulUfX+8FvICFJ1QIbLdjT05kqxJVjBCSsQfMOcIyGl02hvgwrW5xn\n" +
-		"vzvL3ecCs1DUboAZDOstTJX0GOVcf1mHKGOg6gMCgYEAuEcGF1NJYCeaNcF24ATN\n" +
-		"v8B9iEq9WU/Jm/s9EpWdWqVw1UgXr5v/UIg8lTmrMTsfo21UmvMIze/qJxVfYsHA\n" +
-		"XJalSfmOb6qF7W3xwALzR/2vEB5guuglcTXq0DISoUrCZ09D0kzFtxJQ4h0BJpaz\n" +
-		"veEfwPTTPOwqTNY4YZPTlvECgYAcuIvmapzVaRji5p/sz2Vjc/cyxkZ1ccqyhIcK\n" +
-		"7HvhV1ua5LQ7RUKRPm5QZEvHgyO2aKh5LwE1TbR/+OP7PIp85O3o8iO/ELumVYNx\n" +
-		"fFnAH0Y3R7sUy7/kWCdyScmDuYvBIpaDCS+Yqp77dUdPeReLc975mTkc4eB6VaUg\n" +
-		"K9Vl5wKBgQCwThsDhlO4cHR+Ai7ZCe3pfq4t1z01XxNWLAX35T1TkMX4V51fi/MY\n" +
-		"whsrbwTgsyW3Pdma1z5D+39wUYzZ2d+40fB0LPT4Q8b4pSwH7yjt294gwFIQvUlA\n" +
-		"ylf3G/CjUTPvoz2HryxqoSjcYptaHG2F4C767Rx+V6pV4AP1hYp0dg==\n" +
+		"MIIEowIBAAKCAQEAn7DJ1LzFa09rLOJAZlMWXu2/TRXe24b+73FLXDl0g/3clnqX\n" +
+		"snuPWwLlUJ9YPr5EuVTAK09kStuhU+oxr9sxcyWlaQE+QX3uweYiKELK+pPRb3Bl\n" +
+		"D9PmXXeU/tbFa+ey2sDMus/VWG1I+xG/Zwp13sY5pXK0l9IgMkg8TYj1jHsuxjhP\n" +
+		"hvVi4a7PqnAfAW256zKiM+/3ELWTQZu4JuGKGJ2REJZMTCMz5d5Q/2H2jqPDMymM\n" +
+		"23xyfFVFxHoce6W2+0sJjwSU5sxP6FgZxDR2o5EW/Yhk48/3fty5CrMOueaHtytn\n" +
+		"dhYineLIXcz24VM6JKkj0zf6rmctRrXTDFXwcQIBJQKCAQB9KbLy2SUcwbTd/XC+\n" +
+		"5y01pZXwT2l7oSit1Va959fdExStS1RNnx1VK8h9dfkOlSEbo0qCz4X1e7XMJkKe\n" +
+		"lwsVD6soyZ9/cIqzpol5gHWT91Ef/iWydaa39fFSHfSuhStRgltGT96RammiMIFC\n" +
+		"7IXl9U/G3V0dDHoZkpAhGFHBIQcwhhQBlDSLy4YmdQBCAey0hNkztYOfrUraeS61\n" +
+		"ojvKsKEXBdahWw3NPgLjtctDo5xPpACFspPc2m7GrUs2W7HF8GeTcp3WtEJP1Mpc\n" +
+		"hYey3RTzz4aIuTiuNHUWoKnpuF92OdCp9aXmROjoLIiiTagNgi8Tm0A+fJM9LjV3\n" +
+		"1fedAoGBAOieGPUZYARlziO+PUxAY5sE2UDNj10m1PUH+Q91wI7OtLAFw7aBBwXY\n" +
+		"zo+iZMpVNTv8qB0052yX/AzotWxSk++ky7lH3v6l7I9d0axEjuyZO6K5vm3JTyyI\n" +
+		"Yv2ZokHaDoK70HsjIAEr2YyPzqQhvdTsM+Sfi2kjhTbrx+Vq1ULNAoGBAK++Ey8/\n" +
+		"blk8Ru8cPHFSVx0I3AWo0UpBdkeM0xTSN4LdX8zknv+Ke96mGOnQh/y/0r/IEUus\n" +
+		"jd9O++lRCuZE0cE2pN3I0SUmHROpFdU+kT4C0XQD5IBsEA/jP6GjpOtC4/Ud5QVo\n" +
+		"dhjQqHyUHce7ipeizyZy71E7KInyY4YaQ4w1AoGBAIpQKoPlkwmIpBVArtNkjkB/\n" +
+		"bGu4fsHED+uj4DK0t67bxWG+PQS7a/Wjgb0wIED0ZNePTzP18WMp79BTBBbk/gQc\n" +
+		"zCj6TT13ahAORVGsOU5o8wbPTqIkq53wOtv6fCcntZnXdQrySmGPscky6ZIGOYWF\n" +
+		"hqOdInXdxNSMMasBQIiVAoGBAKF+OyR/eih8lDWrPnX4oxPC87IsbUsnZcU1TFhS\n" +
+		"eDMQnTjSFZDgHsyYoUWPMNprFLA7Tinc1WVrLKzisBHeX+H63LAgUXwVL3nZpVwr\n" +
+		"qA94NheU38/0mSNbM42dS3Bm+vzrCc2eQwIL2RiIG12XlByjcj8B1P6JY4WuTZ3S\n" +
+		"8fZ1AoGBANJI5iesUxyyDOcdujxkQ1aGguF9DMuauOfkNjSRQM5MFISh99Gtb+9F\n" +
+		"JUNN2VBPEKKVCKVkXw+a1/DwwrJjDBy3Jld+b7T8g4MVzfMOMr/iQxBEUlF1ym/f\n" +
+		"x35olce90ve7X7U1+FcG8YYlyvE7SCS1+TRSECnQ/FD1WpcBWhnR\n" +
 		"-----END RSA PRIVATE KEY-----\n"
 	file.WriteString(key)
 	return nil, func() error {
