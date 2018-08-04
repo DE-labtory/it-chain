e51f96dedc13a6ffcc5362a79596ca022d16c7dc
zerofruit <14wnrkim@gmail.com>
2018-08-04 11:07:35 +0900

    add block repository

--- blockchain/block.go
+++ blockchain/block.go
@@ -229,3 +229,11 @@ func StageBlock(block Block) error {
 	// save it to event store
 	return nil
 }
+
+type BlockRepository interface {
+	Save(block DefaultBlock) error
+	FindLast() (DefaultBlock, error)
+	FindByHeight(height BlockHeight) (DefaultBlock, error)
+	FindBySeal(seal string) (DefaultBlock, error)
+	FindAll() ([]DefaultBlock, error)
+}
