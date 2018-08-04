efea7f8d3f2780ff19f81b496f82cb4377037f21
KimJunHui <junhui820@gmail.com>
2018-08-04 16:39:20 +0900

    Edited consensus factory test.

--- consensus/consensus_factory_test.go
+++ consensus/consensus_factory_test.go
@@ -19,10 +19,6 @@ package consensus
 import (
 	"testing"
 
-	"github.com/it-chain/engine/common/event"
-	"github.com/it-chain/engine/consensus/test/mock"
-	"github.com/it-chain/engine/core/eventstore"
-	"github.com/it-chain/midgard"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -36,28 +32,22 @@ func TestCreateConsensus(t *testing.T) {
 		Body: make([]byte, 0),
 	}
 
-	eventRepository := mock.MockEventRepository{}
-	eventRepository.SaveFunc = func(aggregateID string, events ...midgard.Event) error {
-		return nil
-	}
-	eventstore.InitForMock(eventRepository)
-
-	p = append(p, l)
-	p = append(p, m)
-
 	// when
-	eventRepository.SaveFunc = func(aggregateID string, events ...midgard.Event) error {
-		assert.Equal(t, len(p), len(events[0].(*event.ConsensusCreated).Representatives))
-		assert.NotNil(t, events[0].(*event.ConsensusCreated).Seal)
-		return nil
-	}
-	eventstore.InitForMock(eventRepository)
-
 	c, err := CreateConsensus(p, b)
 
 	// then
-	assert.Nil(t, err)
+	assert.Error(t, err)
+
+	// when
+	p = append(p, l)
+	p = append(p, m)
+
+	c, err = CreateConsensus(p, b)
+
+	// then
+	assert.NoError(t, err)
 	assert.Equal(t, 2, len(c.Representatives))
+	assert.Equal(t, b.Seal, c.Block.Seal)
 	assert.Equal(t, b.Body, c.Block.Body)
 }
 
@@ -79,20 +69,11 @@ func TestConstructConsensus(t *testing.T) {
 		},
 	}
 
-	eventRepository := mock.MockEventRepository{}
-
 	// when
-	eventRepository.SaveFunc = func(aggregateID string, events ...midgard.Event) error {
-		assert.Equal(t, len(r), len(events[0].(*event.ConsensusCreated).Representatives))
-		assert.Equal(t, "consensusID", events[0].(*event.ConsensusCreated).GetID())
-		return nil
-	}
-	eventstore.InitForMock(eventRepository)
-
 	c, err := ConstructConsensus(msg)
 
 	// then
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, "consensusID", c.ConsensusID.Id)
 	assert.Equal(t, IDLE_STATE, c.CurrentState)
 	assert.Equal(t, 2, len(c.Representatives))
