865fbbadef6c5e3ca259d161afbf836c31ce928b
jun <leebduk@gmail.com>
2018-08-06 22:39:17 +0900

    Refactored tesseract service, and add test case

---
+++ icode/infra/tesseract/container_service.go
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2018 It-chain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tesseract
+
+import (
+	"errors"
+
+	"encoding/json"
+
+	"github.com/it-chain/engine/icode"
+	"github.com/it-chain/tesseract"
+	"github.com/it-chain/tesseract/container"
+	"github.com/it-chain/tesseract/pb"
+	"github.com/rs/xid"
+)
+
+var ErrContainerDoesNotExist = errors.New("container does not exist")
+
+type ContainerService struct {
+	containerMap map[tesseract.ContainerID]tesseract.Container
+}
+
+func NewContainerService() *ContainerService {
+
+	return &ContainerService{
+		containerMap: make(map[tesseract.ContainerID]tesseract.Container),
+	}
+}
+
+func (cs ContainerService) StartContainer(meta icode.Meta) error {
+
+	conf := tesseract.ContainerConfig{
+		Name:      meta.RepositoryName,
+		Directory: meta.Path,
+	}
+
+	container, err := container.Create(conf)
+
+	if err != nil {
+		return err
+	}
+
+	cs.containerMap[meta.ICodeID] = container
+
+	return nil
+}
+
+func (cs ContainerService) ExecuteRequest(request icode.Request) (icode.Result, error) {
+
+	container, ok := cs.containerMap[request.ICodeID]
+
+	if !ok {
+		return icode.Result{}, ErrContainerDoesNotExist
+	}
+
+	resultCh := make(chan icode.Result)
+	errCh := make(chan error)
+
+	var callback = func(response *pb.Response, err error) {
+
+		if err != nil {
+			resultCh <- icode.Result{
+				Err: err.Error(),
+			}
+		}
+
+		data := make(map[string]string)
+
+		if len(response.Data) != 0 {
+			if err = json.Unmarshal(response.Data, &data); err != nil {
+				errCh <- err
+				return
+			}
+		}
+
+		resultCh <- icode.Result{
+			Err:  response.Error,
+			Data: data,
+		}
+	}
+
+	err := container.Request(tesseract.Request{
+		Uuid:     xid.New().String(),
+		Args:     request.Args,
+		FuncName: request.Function,
+		TypeName: request.Type,
+	}, callback)
+
+	if err != nil {
+		errCh <- err
+	}
+
+	select {
+	case err := <-errCh:
+		return icode.Result{}, err
+	case result := <-resultCh:
+		return result, nil
+	}
+}
+
+func (cs ContainerService) StopContainer(id icode.ID) error {
+
+	container, ok := cs.containerMap[id]
+
+	if !ok {
+		return ErrContainerDoesNotExist
+	}
+
+	err := container.Close()
+
+	if err != nil {
+		return err
+	}
+
+	delete(cs.containerMap, id)
+	return nil
+}
---
+++ icode/infra/tesseract/container_service_test.go
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2018 It-chain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tesseract_test
+
+import (
+	"errors"
+	"testing"
+
+	"os"
+
+	"github.com/it-chain/engine/icode"
+	"github.com/it-chain/engine/icode/infra/tesseract"
+	"github.com/stretchr/testify/assert"
+)
+
+func setContainer(t *testing.T) (*tesseract.ContainerService, func()) {
+
+	GOPATH := os.Getenv("GOPATH")
+
+	if GOPATH == "" {
+		t.Fatal(errors.New("need go path"))
+		return nil, func() {}
+	}
+
+	containerService := tesseract.NewContainerService()
+
+	meta := icode.Meta{
+		ICodeID:        "1",
+		RepositoryName: "test icode",
+		Path:           GOPATH + "/src/github.com/it-chain/engine/icode/mock/",
+	}
+
+	err := containerService.StartContainer(meta)
+	assert.NoError(t, err)
+
+	return containerService, func() {
+		err := containerService.StopContainer(meta.ICodeID)
+		assert.NoError(t, err)
+	}
+}
+
+func TestNewTesseractContainerService(t *testing.T) {
+	_, tearDown := setContainer(t)
+	defer tearDown()
+}
+
+func TestTesseractContainerService_ExecuteRequest(t *testing.T) {
+	cs, tearDown := setContainer(t)
+	defer tearDown()
+
+	// success case
+	result, err := cs.ExecuteRequest(icode.Request{
+		ICodeID:  "1",
+		Function: "initA",
+		Type:     "invoke",
+		Args:     []string{},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, result.Err, "")
+
+	// no corresponding icode id
+	result, err = cs.ExecuteRequest(icode.Request{
+		ICodeID:  "2",
+		Function: "initA",
+		Type:     "invoke",
+		Args:     []string{},
+	})
+	assert.Equal(t, err, tesseract.ErrContainerDoesNotExist)
+
+	// invalid type
+	result, err = cs.ExecuteRequest(icode.Request{
+		ICodeID:  "1",
+		Function: "initA",
+		Type:     "invoke2",
+		Args:     []string{},
+	})
+
+	assert.NotEqual(t, result.Err, "")
+
+	// invalid func
+	result, err = cs.ExecuteRequest(icode.Request{
+		ICodeID:  "1",
+		Function: "initAb",
+		Type:     "invoke",
+		Args:     []string{},
+	})
+
+	assert.NotEqual(t, result.Err, "")
+}
