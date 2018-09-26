6b85d7b5bea9280b389f7e507dbba47124c0c590
ParkHaeSung <hea9549@gmail.com>
2018-09-27 00:51:59 +0900

    add icode command & transaction create in endpoint,transport. add api command api in api_gatewayfx

--- cmd/on/api-gatewayfx/api_gatewayfx.go
+++ cmd/on/api-gatewayfx/api_gatewayfx.go
@@ -45,6 +45,7 @@ var Module = fx.Options(
 		api_gateway.NewConnectionEventListener,
 		NewICodeQueryApi,
 		NewICodeEventHandler,
+		NewIvmHttpApi,
 		api_gateway.NewConnectionQueryApi,
 		api_gateway.NewApiHandler,
 		http.NewServeMux,
@@ -72,6 +73,10 @@ func NewKitLogger() kitlog.Logger {
 
 func NewICodeQueryApi(repository *api_gateway.LevelDbICodeRepository) *api_gateway.ICodeQueryApi {
 	return api_gateway.NewICodeQueryApi(repository)
+}
+
+func NewIvmHttpApi() *api_gateway.ICodeCommandApi {
+	return api_gateway.NewICodeCommandApi()
 }
 
 func NewICodeEventHandler(repository *api_gateway.LevelDbICodeRepository) *api_gateway.ICodeEventHandler {
