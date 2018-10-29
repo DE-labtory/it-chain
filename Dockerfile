8acff59762977e5078ef216387c25eb5682b2e3f
jun <leebduk@gmail.com>
2018-10-30 02:08:43 +0900

    Remove unused docker or script files

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM golang:1.9
-ADD . /go/src/github.com/it-chain/engine
-WORKDIR /go/src/github.com/it-chain/engine
-ENV GOBIN=/go/bin
-CMD ["./start.sh"]
--- default_setup.sh
+++
@@ -1,3 +0,0 @@
-go build -buildmode=plugin -o /go/icode.so /icode/icode.go
-go build -o /go/cellcode /go/src/github.com/it-chain/tesseract/cellcode/cellcode.go
-/go/cellcode /go/icode.so $1
